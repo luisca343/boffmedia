090c54657ea686bd1dc589a540658f40eb683014
Luis Carballo <luisca343@gmail.com>
2024-03-09 14:12:58 +0100

    Corrected origins

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { DuplicateEntryExceptionFilter } from './filters/DuplicateEntryException
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  let origin = ['http://localhost:3000', 'http://148.251.3.244:34333', 'http://lizardon.es']
+  let origin = ['http://localhost:3000', 'http://148.251.3.244:34333', 'https://lizardon.es', 'https://boffmedia.es']
   app.enableCors({ origin }); // Enable CORS for the specified origin
 
   const httpServer = app.getHttpServer();
