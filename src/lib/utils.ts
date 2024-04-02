d4310c84a20e62db0c453c4ba7064932f6bb85f4
Luis Carballo <luisca343@gmail.com>
2024-04-02 22:39:54 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function strToDate(date: string) {
+  const dateObj =  new Date(date)
+  return dateObj.toLocaleDateString() + " " + dateObj.toLocaleTimeString()
+}
