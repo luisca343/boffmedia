13761d0c43fc653b2e2bd9f3aab40e7457a6acad
Luis Carballo <luisca343@gmail.com>
2024-04-17 15:58:44 +0200

    Added localization

--- src/app/smartrotom/pokedex/dexUtils.tsx
+++ src/app/smartrotom/pokedex/dexUtils.tsx
@@ -3,3 +3,7 @@ export default function getPokemonSprite(name: string, form: string, shiny: bool
     const folderString = shiny ? 'Front Shiny' : 'Front'
     return `/smartrotom/img/sprites/${folderString}/${name.toUpperCase()}${formString}.png`
 }
+
+export function getPokemonName(name: string, t: any){
+    return t(`pixelmon_${name.toLocaleLowerCase()}`)
+}
