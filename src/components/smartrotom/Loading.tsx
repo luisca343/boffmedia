1d4089014f1ccc074540ba8a427733e6cfbf629d
Luis Carballo <luisca343@gmail.com>
2024-04-22 22:18:59 +0200

    Apply width and height to Loading

--- src/components/smartrotom/Loading.tsx
+++ src/components/smartrotom/Loading.tsx
@@ -1,6 +1,6 @@
-export function Loading() {
+export function Loading({width= 100, height=100}) {
     return (
-        <div className="w-full h-full flex justify-center items-center">
+        <div className="w-full h-full flex justify-center items-center" style={{width, height}}>
             <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-400"></div>
         </div>
     );
