437fcf1da3c4933b5da4de56fed8807aa6c06cc3
Luis Carballo <luisca343@gmail.com>
2024-04-22 22:18:02 +0200

    Allow classes to MenuWrapper

--- src/app/smartrotom/mina/_components/MenuWrapper.tsx
+++ src/app/smartrotom/mina/_components/MenuWrapper.tsx
@@ -1,4 +1,4 @@
-export default function MenuWrapper({children, className} : {children: React.ReactNode, className?: string}) {
+export default function MenuWrapper({children, className =''} : {children: React.ReactNode, className?: string}) {
     return (
         <div className={`relative h-full bg-cover bg-center noSelect ${className}`} style={{backgroundImage: "url('/smartrotom/img/fondoMina.avif')", fontFamily: 'Minecrafter'}}>
             {children}
