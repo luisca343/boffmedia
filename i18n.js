0c6163e9fc60632674ad5c0ac8f5ab9bc44b1869
Luis Carballo <luisca343@gmail.com>
2024-04-22 22:16:49 +0200

    Restructured translations

--- i18n.js
+++ i18n.js
@@ -2,9 +2,10 @@ module.exports = {
   locales: ["es", "ca", "en"],
   defaultLocale: "es",
   pages: {
-    "smartrotom/pokedex": ["smartrotom/pokedex/common"],
-    "smartrotom/pokedex/*": ["smartrotom/pokedex/common"],
-    "*": ["common", "smartrotom/pokedex/common"]
+    "/smartrotom/pokedex": ["smartrotom/pokedex/common"],
+    "/smartrotom/pokedex/*": ["smartrotom/pokedex/common"],
+    "/smartrotom/pokedex/entrada/[[...params]]": ["smartrotom/pokedex/common", "smartrotom/pokedex/moves", "smartrotom/pokedex/spawns", "smartrotom/pokedex/forms"],
+    "*": ["common"]
   },
   loadLocaleFrom: (lang, ns) =>
     import(`/public/locales/${lang}/${ns}.json`).then((m) => m.default),
