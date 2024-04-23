ae0f92d86988037328b8f2a024a611580becdc80
Luis Carballo <luisca343@gmail.com>
2024-04-23 23:14:20 +0200

    Added placeholder background to main

--- src/app/smartrotom/page.tsx
+++ src/app/smartrotom/page.tsx
@@ -3,7 +3,9 @@ import {AppList} from "@/components/smartrotom/AppLink";
 
 export default async function SmartrotomHome() {
   return (
-    <main className="w-full bg-wingull bg-center bg-no-repeat bg-cover bg-fixed flex flex-col">
+    <main className="w-full bg-wingull bg-center bg-no-repeat bg-cover bg-fixed flex flex-col"
+      style={{ backgroundImage: "url('/smartrotom/img/fondoMina.avif')" }}
+    >
       <Hora className="p-5 text-white text-7xl lg:text-8xl col-span-6 text-center text-shadow-gray-border3" />
           <AppList />
     </main>
