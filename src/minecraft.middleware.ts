77f6b86f44875ce5d1a4da6a019aa77bc385fa07
Luis Carballo <luisca343@gmail.com>
2024-04-26 17:08:59 +0200

    Removed logging

--- src/minecraft.middleware.ts
+++ src/minecraft.middleware.ts
@@ -5,12 +5,12 @@ import { Request, Response, NextFunction } from 'express';
 export class MinecraftMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     if(req.method === 'GET' || req.body.server === process.env.MC_WORLD) {
-      console.log(`Accessing ${req.method} ${req.url}`);
-      if(req.body) console.log(req.body);
+      //console.log(`Accessing ${req.method} ${req.url}`);
+      //if(req.body) console.log(req.body);
       next();
     } else {
-      console.log(`Failed to access ${req.method} ${req.url}`);
-      console.log(req.body);
+      //console.log(`Failed to access ${req.method} ${req.url}`);
+      //console.log(req.body);
       res.status(403).send({message: 'You are not authorized to access this route.'});
     }
   }
