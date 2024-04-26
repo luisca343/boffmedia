d17e694deb85e4a78f1596bed38520108ec99f75
Luis Carballo <luisca343@gmail.com>
2024-04-26 17:08:33 +0200

    :sparkles: Added id to PokedexEntry

--- src/_db/schema/Pokedex.ts
+++ src/_db/schema/Pokedex.ts
@@ -2,6 +2,7 @@ import { sql } from "drizzle-orm";
 import { char, datetime, int, mysqlTable } from "drizzle-orm/mysql-core";
 
 export const pokedexRegistry = mysqlTable("pokedex", {
+    id: int("id").primaryKey().autoincrement(),
     uuid: char("uuid", { length: 36 }).notNull(),
     pokemonId: int("pokemon_id").notNull(),
     formId: int("form_id").notNull(),
