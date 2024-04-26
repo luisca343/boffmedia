8b51f8f44d6222014f60860ca44ad0582e8912cb
Luis Carballo <luisca343@gmail.com>
2024-04-26 17:07:18 +0200

    Added Header

--- src/app/smartrotom/pokedex/page.tsx
+++ src/app/smartrotom/pokedex/page.tsx
@@ -1,3 +1,4 @@
+import MenuHeader from "./_components/MenuHeader";
 import PokemonSearchBar from "./_components/PokemonSearchBar";
 import useTranslation from 'next-translate/useTranslation'
 
@@ -5,10 +6,8 @@ export default function PokedexMenu(){
     const { t } = useTranslation("smartrotom/pokedex/common")
     return (
         <div className="bg-gray-800">
-            <header className="flex flex-col bg-slate-950 text-white h-12 z-10 p-2 text-xl 2xl:text-lg" >
-            </header>
+            <MenuHeader />
             <PokemonSearchBar />
-            {t('search')}
         </div>
     )
 }
