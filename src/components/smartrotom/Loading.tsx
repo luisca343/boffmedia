f0ef79b12e1419074e8129c137f8a1fabdde8178
Luis Carballo <luisca343@gmail.com>
2024-04-27 23:01:54 +0200

    Update Loading.tsx

--- src/components/smartrotom/Loading.tsx
+++ src/components/smartrotom/Loading.tsx
@@ -5,3 +5,12 @@ export function Loading({width= 100, height=100}) {
         </div>
     );
 }
+
+
+export function LoadingScreen() {
+    return (
+        <div className="w-full h-full flex justify-center items-center bg-primary-400">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-800"></div>
+        </div>
+    );
+}
