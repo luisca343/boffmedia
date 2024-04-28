8af7f582591d5efaaf1ca9fe17a3f89017fb6f74
Luis Carballo <luisca343@gmail.com>
2024-04-28 17:37:52 +0200

    Possible Spawns

---
+++ src/app/smartrotom/pokedex/spawns/page.tsx
@@ -0,0 +1,10 @@
+import { PokedexSection } from "../_components/PokedexSection";
+import { PossibleSpawns } from "../_components/PossibleSpawns";
+
+export default function Spawns(){
+    return(
+        <div className="bg-gray-800  ">
+            <PossibleSpawns />
+        </div>
+    )
+}
