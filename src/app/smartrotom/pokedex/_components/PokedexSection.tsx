29f719bf3c0d23624cfc897776b3c4942ef28778
Luis Carballo <luisca343@gmail.com>
2024-04-28 17:36:08 +0200

    :sparkles: Added shadcn collapsible

--- src/app/smartrotom/pokedex/_components/PokedexSection.tsx
+++ src/app/smartrotom/pokedex/_components/PokedexSection.tsx
@@ -1,6 +1,9 @@
-export function PokedexSection({title, children, id, className=''}: {title: string, children: any, id?: string, className?: string}){
-    return <section className={`flex flex-col justify-center w-[95%] 2xl:w-[90%] m-auto ${className} `} id={id}>
-        <div className="text-2xl border-b-2 2xl:border-b border-white  mb-4 mt-2 text-white">{title}</div>
-        {children}
-    </section>
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+
+export function PokedexSection({title, children, id, className='', btn=''}: {title: string, children: any, id?: string, className?: string, btn?: any}){
+    return <Collapsible defaultOpen={true} className={`flex flex-col justify-center w-[95%] 2xl:w-[90%] m-auto ${className} `} id={id}>
+        <CollapsibleTrigger className="text-2xl border-b-2 2xl:border-b border-white  mb-4 mt-2 text-white text-start">{title}
+         <span className='ml-2 text-sm text-gray-400'>{btn}</span></CollapsibleTrigger>
+        <CollapsibleContent>{children}</CollapsibleContent>
+    </Collapsible>
 }
