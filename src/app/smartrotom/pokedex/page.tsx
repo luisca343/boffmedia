970e8f8ce7f321ba0fdf38785981327c73a93fe9
Luis Carballo <luisca343@gmail.com>
2024-04-28 17:37:44 +0200

    :sparkles: Possible Spawns

--- src/app/smartrotom/pokedex/page.tsx
+++ src/app/smartrotom/pokedex/page.tsx
@@ -4,25 +4,26 @@ import { PokedexSection } from "./_components/PokedexSection";
 import PokemonSearchBar from "./_components/PokemonSearchBar";
 import useTranslation from 'next-translate/useTranslation'
 import { PossibleSpawns } from "./_components/PossibleSpawns";
+import Link from "next/link";
 
 export default function PokedexMenu(){
     const { t } = useTranslation("smartrotom/pokedex/common")
     return (
-        <div className="bg-gray-800">
+        <div className="bg-gray-800  ">
             <MenuHeader />
-            <div className=" mt-4">
-                <PokedexSection title="Últimos Registros">
-                    <LastRegistries />
-                </PokedexSection>
-
+            <div className="mt-4">
                 <PokedexSection title="Búsqueda">
                     <PokemonSearchBar />
                 </PokedexSection>
 
-                
-                <PokedexSection title="Posibles Spawns">
-                    <PossibleSpawns />
+                <PokedexSection title="Últimos Registros">
+                    <LastRegistries />
                 </PokedexSection>
+                <div >
+                    <PokedexSection title="Posibles Spawns" btn={<Link href={'/smartrotom/pokedex/spawns'}>Ver más</Link>}>
+                        <PossibleSpawns />
+                    </PokedexSection>
+                </div>
             </div>
         </div>
     )
