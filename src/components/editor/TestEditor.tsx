d7dfe9afea16d0c0847a3145dcd4465535479f95
Luis Carballo <luisca343@gmail.com>
2024-04-29 12:09:53 +0200

    :sparkles: Added CKEditor custom build

--- src/components/editor/TestEditor.tsx
+++ src/components/editor/TestEditor.tsx
@@ -1,32 +1,89 @@
-"use client"
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import { Component } from 'react';
-import Editor from './ckeditor'
+import React from 'react';
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import Editor from "ckeditor5-custom-build";
+import './styles.css'
 
-
-export default function TestEditor() {
-        return (
-            <div className="App">
-                <h2>Using CKEditor&nbsp;5 build in React</h2>
-                <CKEditor
-                    editor={ Editor }
-                    data="<p>Hello from CKEditor&nbsp;5!</p>"
-                    onReady={ editor => {
-                        // You can store the "editor" and use when it is needed.
-                        console.log( 'Editor is ready to use!', editor );
-                    } }
-                    onChange={ ( event ) => {
-                        console.log( event );
-                    } }
-                    onBlur={ ( event, editor ) => {
-                        console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        console.log( 'Focus.', editor );
-                    } }
-                />
-            </div>
-        );
-    
+const editorConfiguration = {
+    toolbar: {
+        items: [
+            'heading',
+            '|',
+            'bold',
+            'underline',
+            'strikethrough',
+            'removeFormat',
+            'fontSize',
+            'fontColor',
+            'fontBackgroundColor',
+            'fontFamily',
+            'findAndReplace',
+            '|',
+            'todoList',
+            'bulletedList',
+            'numberedList',
+            'alignment',
+            'outdent',
+            'indent',
+            'subscript',
+            'superscript',
+            '|',
+            'imageInsert',
+            'imageUpload',
+            'blockQuote',
+            'insertTable',
+            'mediaEmbed',
+            'undo',
+            'redo',
+            'code',
+            'selectAll',
+            'codeBlock',
+            'htmlEmbed',
+            'sourceEditing',
+            'style',
+            '|',
+            'pageBreak',
+            'showBlocks',
+            'horizontalLine',
+            'specialCharacters',
+            'restrictedEditingException',
+            'accessibilityHelp'
+        ]
+    },
+    language: 'es',
+    image: {
+        toolbar: [
+            'imageTextAlternative',
+            'toggleImageCaption',
+            'imageStyle:inline',
+            'imageStyle:block',
+            'imageStyle:side'
+        ]
+    },
+    table: {
+        contentToolbar: [
+            'tableColumn',
+            'tableRow',
+            'mergeTableCells',
+            'tableCellProperties'
+        ]
     }
+};
+
+// @ts-ignore
+function CustomEditor( props ) {
+    console.log(Editor)
+        return (
+            <CKEditor
+                // @ts-ignore
+                editor={ Editor.Editor }
+                config={ editorConfiguration }
+                data={ props.initialData }
+                onChange={ (event, editor ) => {
+                    const data = editor.getData();
+                    console.log( { event, editor, data } );
+                } }
+            />
+        )
+}
+
+export default CustomEditor;
