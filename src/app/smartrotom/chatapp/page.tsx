2b37c859bbedfab1c04140b27a4081da655214f8
Luis Carballo <luisca343@gmail.com>
2024-04-29 13:40:26 +0200

    :sparkles: Placeholder

--- src/app/smartrotom/chatapp/page.tsx
+++ src/app/smartrotom/chatapp/page.tsx
@@ -1,8 +1,8 @@
-import TestEditor from "@/components/editor/TestEditor";
+
 import { Loading } from "@/components/smartrotom/Loading";
 
 export default function ChatApp() {
     return (
-        <TestEditor />
+        <Loading />
     );
 }
