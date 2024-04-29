a67354142aa0695f5f1fc78a88061259a39b3fbc
Luis Carballo <luisca343@gmail.com>
2024-04-29 18:08:53 +0200

    :sparkles: Fixed docker build locally

--- i18n.js
+++ i18n.js
@@ -2,11 +2,9 @@ module.exports = {
   locales: ["es", "ca", "en"],
   defaultLocale: "es",
   pages: {
-    "/smartrotom/pokedex": ["smartrotom/pokedex/common"],
-    "/smartrotom/pokedex/*": ["smartrotom/pokedex/common"],
     "/smartrotom/pokedex/entrada/[[...params]]": ["smartrotom/pokedex/common", "smartrotom/pokedex/moves", "smartrotom/pokedex/spawns", "smartrotom/pokedex/forms"],
     "*": ["common"]
   },
   loadLocaleFrom: (lang, ns) =>
-    import(`/public/locales/${lang}/${ns}.json`).then((m) => m.default),
+    import(`/locales/${lang}/${ns}.json`).then((m) => m.default),
 };
--- src/app/smartrotom/mina/ranking/page.tsx
+++ src/app/smartrotom/mina/ranking/page.tsx
@@ -2,17 +2,19 @@ import { rotomGET } from "@/services/boffAPI";
 import MenuWrapper from "../_components/MenuWrapper";
 
 export default async function Ranking(){
-    const ranking = await rotomGET('/mine/ranking');
+    const ranking = await rotomGET('/mine/ranking')  as any[]
     return(
         <MenuWrapper>
-            {ranking?.map((user: any, i: number) => {
-                return (
-                    <div key={i} className="p-4 border rounded shadow  w-auto flex flex-col items-center m-2  bg-gray-900 bg-opacity-80">
-                        <p className="text-gray-400">{user.username}</p>
-                        <p className="text-gray-400">{user.value}</p>
-                    </div>
-                )
-            })}
+            <div>
+                {ranking?.map((user: any, i: number) => {
+                    return (
+                        <div key={i} className="p-4 border rounded shadow  w-auto flex flex-col items-center m-2  bg-gray-900 bg-opacity-80">
+                            <p className="text-gray-400">{user.username}</p>
+                            <p className="text-gray-400">{user.value}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </MenuWrapper>
     )
 }
