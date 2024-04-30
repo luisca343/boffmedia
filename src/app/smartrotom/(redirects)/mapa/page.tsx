15992ad5bd19c238222958bb2b880480653e9835
Luis Carballo <luisca343@gmail.com>
2024-04-30 14:03:56 +0200

    :sparkles: Fixed map url

--- src/app/smartrotom/(redirects)/mapa/page.tsx
+++ src/app/smartrotom/(redirects)/mapa/page.tsx
@@ -1,5 +1,5 @@
 import WebExterna from "@/components/smartrotom/WebExterna";
 
 export default function MapaWingull(){
-    return <WebExterna url='https:/mapawingull.lizardon.es' />
+    return <WebExterna url='https://mapawingull.boffmedia.es' />
 }
