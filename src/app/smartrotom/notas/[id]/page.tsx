edf82432289c17233c637fc25e8a040804d2273d
Luis Carballo <luisca343@gmail.com>
2024-05-02 18:37:43 +0200

    :sparkles: Load document from database

--- src/app/smartrotom/notas/[id]/page.tsx
+++ src/app/smartrotom/notas/[id]/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
+import { rotomGET } from '@/services/boffAPI';
 
 const CustomEditor = dynamic( () => {
   return import( '@/components/editor/TestEditor' );
@@ -9,12 +10,23 @@ const CustomEditor = dynamic( () => {
 
 export default function Note({params} : {params: {id: string}}){
   const { id } = params;
+  const [data, setData] = React.useState<any>(null);
+
+  useEffect(() => {
+    const data = rotomGET(`/documents/${id}`)
+    .then((res) => {
+      setData(res.content);
+      //rewrite url
+    });
+    
+  }, [id]);
 
   return (
   <div className='h-full'>
   <CustomEditor
-    initialData='Título de tu documento'
+    initialData={data}
     documentId={id}
+    documentType={0}
   /></div>
   );
 }
