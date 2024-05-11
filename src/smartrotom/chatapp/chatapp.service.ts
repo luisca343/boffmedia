f7bcd7393d3ef8abeee43bbf028a6e52bdd971d0
Luis Carballo <luisca343@gmail.com>
2024-05-11 15:39:31 +0200

    :sparkles: Get chat members

--- src/smartrotom/chatapp/chatapp.service.ts
+++ src/smartrotom/chatapp/chatapp.service.ts
@@ -6,7 +6,8 @@ import { last } from 'rxjs';
 import { SocketsGateway } from '../sockets/sockets.gateway';
 import { smartrotomUsers } from '@/_db/schema/SmartRotom';
 import { ResultSetHeader } from 'mysql2';
-import { group } from 'console';
+import { error, group } from 'console';
+import { uuid } from 'drizzle-orm/pg-core';
 
 @Injectable()
 export class ChatappService {
@@ -27,7 +28,7 @@ export class ChatappService {
 
 
        const groups = await Promise.all(userGroups.map(async (group: {id: number, name: string, type: number, description: string, 
-            image: string, createdAt: Date, updatedAt: Date, messages: {id: number, content: string, createdAt: Date}[], unread: number}) => {
+            image: string, createdAt: Date, updatedAt: Date, messages: {id: number, content: string, createdAt: Date}[], unread: number, members: {uuid: string}[]}) => {
             const messages = (await this.db.getDrizzle()
                 .select({id: rotomChatMessages.id, content: rotomChatMessages.content, createdAt: rotomChatMessages.createdAt, uuid: rotomChatMessages.senderUUID})
                 .from(rotomChatMessages)
@@ -35,20 +36,30 @@ export class ChatappService {
                 .orderBy(desc(rotomChatMessages.createdAt))
                 .limit(50))
 
-                const chatName = group.type == 0 ? group.name : group.name.split('_').filter((name) => name !== uuid)[0] || 'Mensajes guardados'
-                const chatImage = group.type == 0 ? `/smartrotom/img/apps/chatapp/${group.image}` : chatName === 'Mensajes guardados' ? `https://crafatar.com/avatars/${uuid}` : `https://crafatar.com/avatars/${chatName}`
+                const members = await this.getChatMembers(group.id)
+
+                const otherPlayerUUID = group.name.split('_').filter((name) => name !== uuid)[0]
+                const otherPlayerName = (await this.db.getDrizzle().select({name: smartrotomUsers.username}).from(smartrotomUsers).where(eq(smartrotomUsers.uuid, otherPlayerUUID)))[0]?.name || 'Mensajes guardados'
+                
+                const chatName = group.type == 0 || group.type == 3 ? group.name : otherPlayerName || 'Mensajes guardados'
+                const imageName = group.image || 'default.webp'
+                const chatImage = group.type == 0 || group.type == 3  ? `/smartrotom/img/apps/chatapp/${imageName}` : chatName === 'Mensajes guardados' ? `https://crafatar.com/avatars/${uuid}` : `https://crafatar.com/avatars/${otherPlayerUUID}`
                 
                 group.messages = messages || [{id: 0, content: '', createdAt: null}]
                 group.name = chatName
                 group.image = chatImage
                 group.unread = 0
+                group.members = members
 
                 return group
         }))
 
 
         groups.sort((a, b) => {
-            return a.messages[0]?.createdAt.getTime() - b.messages[0]?.createdAt.getTime()
+            const aDate = a.messages[0]?.createdAt || new Date()
+            const bDate = b.messages[0]?.createdAt || new Date()
+
+            return bDate.getTime() - aDate.getTime()
         })
         
         return groups
@@ -111,40 +122,73 @@ export class ChatappService {
     }
 
     async createChat(player: string, users: string[], name: string){
+        console.log('CREATING CHAT')
+
+        const chatUsers = new Set(users)
+        chatUsers.add(player)
+
+        const uuids = Array.from(chatUsers)
+        console.log(uuids)
+
         let chatName = name
         let chatType = 1
-        if(users.length == 1){
-            const uuids = [player, users[0]]
-            uuids.sort()
-            chatName = uuids.join('_')
+
+        if(uuids.length == 1){
+            /*
             const exists = await this.db.getDrizzle().select({id: rotomChats.id}).from(rotomChats)
                 .where(eq(rotomChats.name, chatName))
     
             if(exists.length > 0){
                 return exists[0].id
-            } 
+            } */
 
+            chatType = 1
+
+            console.log('Creating single chat')
+            console.log(chatName)
+        } else if (uuids.length == 2){
+            uuids.sort()
+            chatName = uuids.join('_')
             chatType = 2
+
+            const chatExists = await this.db.getDrizzle().select({id: rotomChats.id}).from(rotomChats)
+                .where(eq(rotomChats.name, chatName))
+
+            if(chatExists.length > 0){
+                console.log('Chat already exists')
+                return chatExists[0].id
+            }
+            
+            
+
+            console.log('Creating private chat')
+            console.log(chatName)
+        } else if (uuids.length > 2){
+            chatType = 3
+            console.log('Creating group chat')
+            console.log(chatName)
         }
-        
 
         const newChat = await this.db.getDrizzle().insert(rotomChats)
             .values({type: chatType, name: chatName, description: 'Chat'}) as ResultSetHeader[]
 
         const insertId = newChat[0].insertId
 
-        await this.db.getDrizzle().insert(rotomChatUsers)
-            .values({chatId: insertId, uuid: player})
-
-
-        if(chatType == 1){
-            users.forEach(async (uuid) => {
-                await this.db.getDrizzle().insert(rotomChatUsers)
-                    .values({chatId: insertId, uuid})
-            })
-        }
+        uuids.forEach(async (uuid) => {
+            await this.db.getDrizzle().insert(rotomChatUsers)
+                .values({chatId: insertId, uuid})
+        })
 
         return insertId
+    }
+
+    async call(chatId: number, uuid: string){
+        let users = await this.getChatMembers(chatId)
+        users = users.filter((user: {uuid: string}) => user.uuid !== uuid)
+        
+        if(users.length == 0) return {error: 'No users in chat'}
+
+        return 1
     }
     
 }
