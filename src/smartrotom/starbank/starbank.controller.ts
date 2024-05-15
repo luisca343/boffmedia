66d479128b329a10e82772a4680fdf22e1d96b1d
Luis Carballo <luisca343@gmail.com>
2024-05-15 17:44:00 +0200

    :sparkles: Communication with the MC server

--- src/smartrotom/starbank/starbank.controller.ts
+++ src/smartrotom/starbank/starbank.controller.ts
@@ -18,9 +18,19 @@ export class StarbankController {
         return this.starbankService.getAccounts(uuid);
     }
 
+    @Post("trainerdefeat")
+    trainerDefeat(@Body() body: {money: number, uuid: string}) {
+        return this.starbankService.trainerDefeat(body.money, body.uuid);
+    }
+
     @Post("shop")
     shop(@Body() body: {uuid: string, npcName: string, itemName: string, operation: string, unitPrice: number, count: number}){
         return this.starbankService.shop(body);
+    }
+
+    @Post("transfer")
+    transfer(@Body() body: {from: number, to: number, amount: number, concept: string}){
+        return this.starbankService.transfer(body.from, body.to, body.amount, body.concept);
     }
 
     @Get("transactions/:uuid")
--- src/smartrotom/starbank/starbank.service.ts
+++ src/smartrotom/starbank/starbank.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@nestjs/common';
 import { desc, eq, or } from 'drizzle-orm';
 import { alias } from 'drizzle-orm/mysql-core';
 import { RowDataPacket } from 'mysql2';
+import axios from 'axios';
 
 @Injectable()
 export class StarbankService {
@@ -63,6 +64,58 @@ export class StarbankService {
         const insert = res2[0];
         const res3 = await this.db.getDrizzle().insert(starBankUsersAccounts).values({uuid, accountId: insert.insertId}).execute();
         return {success: true}
+    }
+
+    async getAccountInfo(accountId: number) {
+        return await this.db.getDrizzle()
+            .select({balance: starBankAccounts.balance, type: starBankAccounts.type, uuid: starBankUsersAccounts.uuid})
+            .from(starBankAccounts)
+            .leftJoin(starBankUsersAccounts, eq(starBankAccounts.id, starBankUsersAccounts.accountId))
+            .where(eq(starBankAccounts.id, accountId));
+    }
+    
+    async transfer(from: number, to: number, amount: number, concept: string) {
+        const trans = await this.transaction(from, to, amount, concept, "TRANSFERENCIA");
+    
+        const fromAccount = await this.getAccountInfo(from);
+        const toAccount = await this.getAccountInfo(to);
+    
+        const accounts = [fromAccount[0], toAccount[0]];
+    
+        accounts.forEach(async (account) => {
+            if(account.type === "MAIN") {
+                await this.updateBalance(account);
+            }
+        });
+        
+        return trans;
+    }
+    
+
+    updateBalance(account: {balance: number, type: string, uuid: string}) {
+        axios.post(`${process.env.WINGULL_API}/updateBalance`, account)
+            .catch(error => {
+                console.log(error);
+        });
+      
+    }
+
+    async trainerDefeat(amount: number, uuid: string) {
+        const account = await this.getMainAccount(uuid);
+
+        console.log(`Petición a wingull: ${process.env.WINGULL_API}/getCurrentBalance`)
+        const test = await axios.post(`${process.env.WINGULL_API}/getCurrentBalance`, {uuid, amount})
+        const money = test.data as number
+    
+        const prevBalance = account.balance;
+        
+        const diff = money - prevBalance;
+
+        console.log(`Dinero actual: ${money}, Dinero anterior: ${prevBalance}, Diferencia: ${diff}`)
+
+        if(diff !== 0) return this.transaction(0, account.id, diff, "Derrota de entrenador", "ENTRENADOR");
+        return {success: true}
+
     }
 
     async shop(body: {uuid: string, npcName: string, itemName: string, operation: string, unitPrice: number, count: number}) {
