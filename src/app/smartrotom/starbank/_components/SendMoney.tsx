154b4db81e949342ddfdc8dada09795576c20e96
Luis Carballo <luisca343@gmail.com>
2024-05-15 17:45:22 +0200

    :sparkles: Extracted SendMoney to component

--- src/app/smartrotom/starbank/_components/SendMoney.tsx
+++ src/app/smartrotom/starbank/_components/SendMoney.tsx
@@ -1,30 +1,92 @@
 import { useEffect, useState } from "react";
-import { BankSection, BankSectionContent, BankSectionHeader } from "./BankSection";
 import { BoffSession } from "@/components/smartrotom/AppWrapper";
 import { useSession } from "next-auth/react";
-import { rotomGET } from "@/services/boffAPI";
+import { rotomGET, rotomPOST } from "@/services/boffAPI";
+import { SelectCuenta } from "../page";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { toast } from "react-toastify";
 
 export function SendMoney(){
     const { data: session } = useSession() as {data: BoffSession | null}
+    const [myAccounts, setMyAccounts] = useState([] as any)
+    const [myActiveAccount, setMyActiveAccount] = useState(0)
+
     const [accounts, setAccounts] = useState([])
+    const [activeAccount, setActiveAccount] = useState(0)
+
+    const [amount, setAmount] = useState(0)
+    const [concept, setConcept] = useState("")
 
     useEffect(() => {
-        if (session?.user) {
+        if (!session) return
             rotomGET("/starbank/accounts/")
                 .then((res) => {
                     setAccounts(res);
                 });
-        }
+        
+        rotomGET("/starbank/accounts/" + session.user.smartRotomUser.uuid)
+        .then((res) => {
+            setMyAccounts(res);
+        });
     }, [session]);
+
+    useEffect(() => {
+        console.log(activeAccount)
+    }, [activeAccount]);
+
+
+    function sendMoney(){
+        if (activeAccount === 0 || myActiveAccount === 0) {
+            toast.error('Selecciona una cuenta')
+            return
+        }
+        if (activeAccount === myActiveAccount) {
+            toast.error('No puedes enviar dinero a la misma cuenta')
+            return
+        }
+        if (amount <= 0) {
+            toast.error('Ingresa una cantidad válida')
+            return
+        }
+        if(amount > myAccounts.find((account: any) => account.id === myActiveAccount)?.balance) {
+            toast.error('No tienes suficiente saldo')
+            return
+        }
+        const data = {
+            from: myActiveAccount,
+            to: activeAccount,
+            amount: amount,
+            concept: concept
+        }
+        rotomPOST("/starbank/transfer", data)
+        .then((res) => {
+            if(res.error) {
+                toast.error(res.error)
+                return
+            }
+            toast.success('Transferencia realizada')
+            setAmount(0)
+            setConcept("")
+        })
+    }
     
     return(
-        <div className="flex flex-col w-full h-full p-2">
-            <BankSection className="w-[50%] h-full m-auto">
-                <BankSectionHeader >Enviar Dinero </BankSectionHeader>
-                <BankSectionContent>
-                    <></>
-                </BankSectionContent>
-            </BankSection>
+        <div className="h-full flex flex-col justify-evenly items-center">
+            <section className="flex flex-col justify-between items-center w-full">
+            <span>Desde:</span>
+            <SelectCuenta accounts={myAccounts} activeAccount={myActiveAccount} setActiveAccount={setMyActiveAccount}/>
+            <div className="text-xl">
+               {myAccounts.find((account: any) => account.id === myActiveAccount)?.balance}
+            </div>
+                <span>Hacia:</span>
+                <SelectCuenta accounts={accounts} activeAccount={activeAccount} setActiveAccount={setActiveAccount}/>
+            </section>
+            <section className="flex flex-col justify-between items-center w-full">
+                <Input type="number" placeholder="Cantidad" onChange={(e) => setAmount(parseInt(e.target.value))} value={amount}/>
+                <Input type="text" placeholder="Concepto" onChange={(e) => setConcept(e.target.value)} value={concept}/>
+            </section>
+            <Button onClick={() => sendMoney()} className="bg-blue-900 hover:bg-blue-700 text-white ">Enviar</Button>
         </div>
     )
 }
--- src/app/smartrotom/starbank/transacciones/page.tsx
+++ src/app/smartrotom/starbank/transacciones/page.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import { BankSection, BankSectionContent, BankSectionFooter, BankSectionHeader } from "../_components/BankSection";
 import { AccountImage } from "../_components/AccountImage";
 import { Transactions } from "../page";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTrigger } from "@/components/ui/dialog";
+import { SendMoney } from "../_components/SendMoney";
 
 export default function Transacciones(){
     const { data: session } = useSession() as {data: BoffSession | null}
@@ -29,7 +31,17 @@ export default function Transacciones(){
                     <Transactions transactions={transactions} activeAccount={{id: 1}}/>
                 </BankSectionContent>
                 <BankSectionFooter>
-                    <button onClick={() => console.log('Enviar dinero')} className="bg-blue-900 hover:bg-blue-700 text-white p-2 rounded-md">Enviar dinero</button>
+                    <Dialog>
+                        <DialogTrigger>
+                            <button className="bg-blue-900 hover:bg-blue-700 text-white p-2 rounded-md">Enviar dinero</button>
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>Enviar dinero</DialogHeader>
+                            <DialogDescription>
+                                <SendMoney />
+                            </DialogDescription>
+                        </DialogContent>
+                    </Dialog>
                 </BankSectionFooter>
             </BankSection>
         </div>
