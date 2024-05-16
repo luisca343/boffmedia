93b708f5f2048b09e24d47e9b92bf51baae25979
Luis Carballo <luisca343@gmail.com>
2024-05-16 20:38:24 +0200

    :sparkles: Add filter tto SelectCuenta

--- src/app/smartrotom/starbank/page.tsx
+++ src/app/smartrotom/starbank/page.tsx
@@ -9,6 +9,7 @@ import TestChart from "./_components/chart"
 import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from "@/components/ui/select"
 import { BankSection, BankSectionButton, BankSectionContent, BankSectionFooter, BankSectionHeader } from "./_components/BankSection"
 import { AccountImage } from "./_components/AccountImage"
+import { Input } from "@/components/ui/input"
 
 export default function StarBank(){
     const { data: session } = useSession() as {data: BoffSession | null}
@@ -24,7 +25,7 @@ export default function StarBank(){
                     setAccounts(res);
                 });
 
-            rotomGET("/starbank/transactions/" + session.user.smartRotomUser.uuid)
+            rotomGET("/starbank/transactions/" + session.user.smartRotomUser.uuid +"?limit=10")
                 .then((res) => {
                     setTransactions(res);
             });
@@ -113,7 +114,7 @@ export default function StarBank(){
                 <BankSection className="h-1/5">
                     <BankSectionHeader> Transferencias </BankSectionHeader>
                     <BankSectionContent>
-                        <Transactions transactions={transfers} activeAccount={activeAccount} />
+                        <TransfersShort transfers={transfers} activeAccount={activeAccount} />
                     </BankSectionContent>
                     <BankSectionFooter>
                         <BankSectionButton onClick={() => {console.log("click")}}>Ir a Transferencias</BankSectionButton>
@@ -130,14 +131,17 @@ export default function StarBank(){
 
 
 
-export function SelectCuenta({accounts, activeAccount, setActiveAccount}: {accounts: any, activeAccount: any, setActiveAccount: any}){
+export function SelectCuenta({accounts, activeAccount, setActiveAccount, id}: {accounts: any, activeAccount: any, setActiveAccount: any, id?: string}){
+    const [filter, setFilter] = useState("")
+
     return(
         <Select onValueChange={(e) => setActiveAccount(e)}>
-        <SelectTrigger className="w-[180px]">
+        <SelectTrigger>
             <SelectValue placeholder={activeAccount.name} />
         </SelectTrigger>
         <SelectContent>
-            {accounts.map((account: any) => (
+            <Input type="text" placeholder="Buscar cuenta" onChange={(e) => setFilter(e.target.value.toLowerCase())} value={filter} id={id}/>
+            {accounts.filter((account: any) => account.name.toLowerCase().includes(filter) || account.type.toLowerCase().includes(filter)).map((account: any) => (
                 <SelectItem key={account.id} value={account.id}>
                         <div className="flex w-full">
                             <AccountImage width={20} height={20} type={account.type} name={account.name}/>
@@ -150,6 +154,31 @@ export function SelectCuenta({accounts, activeAccount, setActiveAccount}: {accou
     )
 }
 
+export function TransfersShort({transfers, activeAccount}: {transfers: any, activeAccount: any}){
+    return(
+        <div className="flex justify-evenly ">
+            {transfers.map((transfer: any) => {
+                const transactionType = transfer.from === activeAccount.id ? "out" : "in";
+                const amount = transactionType === "out" ? -transfer.amount : transfer.amount;
+                const currentBalance = transactionType === "out" ? transfer.fromBalance : transfer.toBalance;
+                const name = transactionType === "out" ? transfer.toName : transfer.fromName;
+                const type = transactionType === "out" ? transfer.toType : transfer.fromType;
+
+                return (
+                    <div className="flex flex-col justify-center items-center" key={transfer.date} >
+                        <div className="flex hover:bg-opacity-50 items-center my-1">
+                            <AccountImage width={32} type={type} name={name}/>
+                            <div className={`text-right my-auto mx-2 ${esPagador(transfer, activeAccount) ? 'text-red-800' : 'text-green-700'}`}>
+                                <div className="font-bold text-xl text-shadow-border05">{amount} &#165;</div>
+                            </div>
+                        </div>
+                        <div className="flex text-sm text-center">{strToDate(transfer.date)} - {name}</div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
 
 export function Transactions({transactions, activeAccount, className}: {transactions: any, activeAccount: any, className?: string}){
     if(transactions.length === 0){
@@ -160,8 +189,7 @@ export function Transactions({transactions, activeAccount, className}: {transact
         )
     }
     return(
-
-            <div className="flex flex-col overflow-auto h-full p-2">
+            <div className={`flex flex-col overflow-auto h-full p-2 ${className}`}>
             {transactions.map((transaction: any) => {
                 const transactionType = transaction.from === activeAccount.id ? "out" : "in";
                 const amount = transactionType === "out" ? -transaction.amount : transaction.amount;
@@ -170,7 +198,7 @@ export function Transactions({transactions, activeAccount, className}: {transact
                 const type = transactionType === "out" ? transaction.toType : transaction.fromType;
 
                 return (
-                <div key={transaction.date} className="flex  hover:bg-opacity-50 items-center">
+                <div key={transaction.date} className="flex  hover:bg-opacity-50 items-center my-1">
                     <AccountImage type={type} name={name}/>
                     <div className="min-h-9 my-auto mx-4 flex flex-col flex-1">
                         <div className="text-lg font-bold break-all">{transaction.reason}</div>
