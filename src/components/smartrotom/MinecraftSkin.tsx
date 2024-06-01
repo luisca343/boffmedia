96dc574374126d97cd0cfc92b9f79e45c21b1464
Luis Carballo <luisca343@gmail.com>
2024-06-01 16:16:04 +0200

    :sparkles: Store renders of skins

--- src/components/smartrotom/MinecraftSkin.tsx
+++ src/components/smartrotom/MinecraftSkin.tsx
@@ -1,9 +1,13 @@
 
+import { rotomGET, rotomPOST } from "@/services/boffAPI";
 import {  SkinViewer } from "skinview3d"
 
 const skinCache = new Map<string, string>();
 export default async function NpcSkin({npcName, width= 150, height= 150} : {npcName: string, width?: number, height?: number}) {
-    if(!skinCache.has(npcName)) {
+    let skin = await rotomGET(`/img/customNPC/render/${npcName}`)
+    console.log("BUSCANDO RENDER PARA " +npcName + ":", skin)
+    
+    if(skin.error) {
         console.log("Cache miss")
         const skinViewer = new SkinViewer(
             {
@@ -12,10 +16,10 @@ export default async function NpcSkin({npcName, width= 150, height= 150} : {npcN
                 enableControls: false,
             })
         
-            /*
-            skinViewer.width = 200
-            skinViewer.height = 200
-            skinViewer.camera.setViewOffset(200,400, 0, 0, 200, 200)*/
+            
+            //skinViewer.width = 200
+            //skinViewer.height = 200
+            //skinViewer.camera.setViewOffset(200,400, 0, 0, 200, 200)
             
             skinViewer.camera.rotation.x = -0.620;
             skinViewer.camera.rotation.y = 0.534;
@@ -25,18 +29,28 @@ export default async function NpcSkin({npcName, width= 150, height= 150} : {npcN
             skinViewer.camera.position.z = 42.0;
             
 
+            let skin = await rotomGET(`/img/customNPC/${npcName}`)
+            console.log("SE GA DETECTADO LA SKIN", skin)
+            if(skin.error) {
+            await skinViewer.loadSkin(`/smartrotom/img/customNPC/steve.png`)
+            } else {
+                await skinViewer.loadSkin(`/smartrotom/img/customNPC/${npcName}.png`)
+            }
 
-            await skinViewer.loadSkin(`https://api.boffmedia.es/smartrotom/img/customNPC/${npcName}.png`)
             skinViewer.render();
             const image = skinViewer.canvas.toDataURL();
 
             skinCache.set(npcName, image);
+            skin  = {img: image}
+            
+            rotomPOST('/img/customNPC', {npcName, image})
         
 
         skinViewer.dispose();
     }
-    if(!skinCache.get(npcName)) return <div style={{width, height, minWidth:width, minHeight:height}}></div>
+   
+    if(skin.error) return <div style={{width, height, minWidth:width, minHeight:height}}></div>
     return (
-        <img width={width} height={height} src={skinCache.get(npcName)} alt={npcName} />
+        <img width={width} height={height} src={`/smartrotom/img/customNPC/renders/${npcName}.png`} alt={npcName} />
     )
 }
