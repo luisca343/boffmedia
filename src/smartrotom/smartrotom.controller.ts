cf146b1f0eaa299c20c7ee4489afc29d9f5ff658
Luis Carballo <luisca343@gmail.com>
2024-06-01 16:15:21 +0200

    :sparkles: Store NPC renders

--- src/smartrotom/smartrotom.controller.ts
+++ src/smartrotom/smartrotom.controller.ts
@@ -8,13 +8,19 @@ export class SmartrotomController {
 
     @Post("img/customNPC")
     async img(@Body() {npcName, image}: {npcName: string, image: string}){
-        console.log("Received image")
-        fs.writeFileSync(`./public/smartrotom/img/customNPC/${npcName}.png`, image.replace(/^data:image\/png;base64,/, ""), 'base64')
+        fs.writeFileSync(`./public/smartrotom/img/customNPC/renders/${npcName}.png`, image.replace(/^data:image\/png;base64,/, ""), 'base64')
+        return {status: "OK"}
+    }
+    @Get("img/customNPC/:npcName")
+    async get(@Param('npcName') npcName: string){
+        const exists = fs.existsSync(`./public/smartrotom/img/customNPC/${npcName}.png`)
+        if(!exists) return {status: 400, error: "No existe la imagen"}
+        return {status: "OK"}
     }
 
-    @Get("img/customNPC/:npcName")
+    @Get("img/customNPC/render/:npcName")
     async getImg(@Param('npcName') npcName: string){
-        const exists = fs.existsSync(`./public/smartrotom/img/customNPC/${npcName}.png`)
+        const exists = fs.existsSync(`./public/smartrotom/img/customNPC/renders/${npcName}.png`)
         if(!exists) return {error: "No existe la imagen"}
         return {status: "OK"}
     }
