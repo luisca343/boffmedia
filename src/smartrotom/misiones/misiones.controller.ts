f567864807cac5ed7f5c0bf6815c1b7e5f626fe1
Luis Carballo <luisca343@gmail.com>
2024-06-03 21:12:45 +0200

    Default NPCS

--- src/smartrotom/misiones/misiones.controller.ts
+++ src/smartrotom/misiones/misiones.controller.ts
@@ -94,6 +94,7 @@ export class MisionesController {
         if(!this.npcs){
           const newNPCs = await axios.get('http://148.251.3.244:34370/updateNPCs')
           this.npcs = newNPCs.data.npcs
+          this.npcs[0] = {name: "Rotom", dialogId: 0, skin: "rotom"}
         } 
         if(!this.quests && !force) return this.getAllQuests(1)
         if(this.quests && Date.now() - this.lastUpdate < 4 * 60 * 60 * 1000 && !force) {
@@ -381,7 +382,18 @@ export class MisionesController {
                 5,
                 6
             ]
-        }
+        }, "npcs": [
+            {
+                "name": "Rotom",
+                "dialogId": 0,
+                "skin": "rotom"
+            },
+            {
+                "name": "Ficus",
+                "dialogId": 1,
+                "skin": "ficus"
+            }
+        ]
     }
     }
   
