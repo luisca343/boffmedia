e2768b90ff69ff487af465b902cd82a4bbfde6eb
Luis Carballo <luisca343@gmail.com>
2024-06-11 18:48:06 +0200

    Run full battle

--- src/app/battlesim/page.tsx
+++ src/app/battlesim/page.tsx
@@ -12,29 +12,134 @@ import GameCanvas from "./_components/GameCanvas";
 
 let battle = new Battle(new Generations(Dex as any))
 
+const BattleEffects: {[k: string]: any} = {
+  pokeball: {
+    url: '/smartrotom/test/pokeball.png',
+    w: 100, h: 100,
+  },
+};
+
+
+export interface ScenePos {
+	/** - left, + right */
+	x?: number;
+	/** - down, + up */
+	y?: number;
+	/** - player, + opponent */
+	z?: number;
+	scale?: number;
+	xscale?: number;
+	yscale?: number;
+	opacity?: number;
+	time?: number;
+	display?: string;
+}
+
 export class Scene {
   battle: Battle;
   gameElement: HTMLElement;
+  acceleration = 1;
   
 
   constructor(battle: Battle, gameElement: HTMLElement) {
     this.battle = battle
     this.gameElement = gameElement
 
-
-    console.log(`Scene created with battle ${battle}`)
-    console.log(`Scene created with gameElement ${gameElement}`)
+    console.log(battle)
   }
 
   getPosition(id: string) {
     const element = this.gameElement.querySelector(`#${id}`);
     if (!element) return null;
-
+  
     const rect = element.getBoundingClientRect();
-    console.log(`Element: ${id} - ${rect.x}, ${rect.y}, ${rect.width}, ${rect.height}`);
-    return rect;
+    const parentRect = this.gameElement.getBoundingClientRect();
+  
+    const relativeRect = {
+      x: rect.left - parentRect.left + rect.width / 2,
+      y: rect.top - parentRect.top + rect.height / 2,
+      width: rect.width,
+      height: rect.height
+    };
+  
+    console.log(`Element: ${id} - ${relativeRect.x}, ${relativeRect.y}, ${relativeRect.width}, ${relativeRect.height}`);
+    return relativeRect;
   }
+
+  playAnimation(animation: string, position: string) {
+    const element = this.gameElement.querySelector(`#${position}`);
+    if (!element) return null;
+
+    console.log(`Playing animation ${animation} on ${position}`);
+  }
+
+  showEffect(effect:string, start: ScenePos, end: ScenePos, transition: string, after?: string, additionalCss?: string, callback?: () => void){
+    const effectData = BattleEffects[effect];
+    if (!effectData) return;
+
+    start.x = start.x || 0;
+    start.y = start.y || 0;
+
+
+    const element = document.createElement('img');
+    element.src = effectData.url;
+    element.style.position = 'absolute';
+    element.style.width = `50px`;
+    element.style.height = `50px`;
+    const halfWidth = parseInt(element.style.width) / 2;
+    const halfHeight = parseInt(element.style.height) / 2;
+    element.style.left = `${start.x - halfWidth}px`;
+    element.style.top = `${start.y - halfHeight}px`;
+    element.style.zIndex = `${start.z}`;
+    element.style.opacity = `${start.opacity || 1}`;
+    element.style.transition = `all ${start.time || 0}ms`;
+    element.style.display = start.display || 'block';
+    element.style.transform = `scale(${start.scale || 1})`;
+
+    if (additionalCss) {
+      element.style.cssText += additionalCss;
+    }
+
+    this.gameElement.appendChild(element);
+
+    setTimeout(() => {
+      end.x = end.x || 0;
+      end.y = end.y || 0;
+
+      element.style.left = `${end.x - halfWidth}px`;
+      element.style.top = `${end.y - halfHeight}px`;
+      element.style.zIndex = `${end.z}`;
+      element.style.opacity = `${end.opacity || 0}`;
+      element.style.transform = `scale(${end.scale || 1})`;
+      
+    }, 0);
+
+    setTimeout(() => {
+      element.remove();
+      if (callback) callback();
+    }, start.time || 0);
 }
+
+
+/*
+
+		this.scene.showEffect('pokeball', {
+			opacity: 1,
+			x: this.x,
+			y: this.y - 40,
+			z: this.z,
+			scale: .7,
+			time: 300 / this.scene.acceleration,
+		}, {
+			opacity: 0,
+			x: this.x,
+			y: this.y,
+			z: this.behind(50),
+			time: 700 / this.scene.acceleration,
+		}, 'ballistic2');
+
+*/
+
 
 export default function Test() {
   // position is the side + the position in the team
@@ -45,12 +150,13 @@ export default function Test() {
   const [log, setLog] = useState([] as any[])
   const [scene, setScene] = useState({} as Scene)
   
-  const gameElementRef = useRef(null);
+  const logRef = useRef(null);
 
   useEffect(() => {
-  }, []); 
-
-
+    if (logRef.current) {
+      logRef.current.scrollTop = logRef.current.scrollHeight;
+    }
+  }, [log]);
 
   useEffect(() => {
     if (pokemonUpdates.length > 0) {
@@ -87,6 +193,12 @@ export default function Test() {
   }, [])
 
   useEffect(() => {
+    if(turn === 0) return
+    
+    testAttack();
+  }, [turn])
+
+  useEffect(() => {
     if(!partida.log) return
     
     const gameElement = document.querySelector('#game') as HTMLElement;
@@ -110,6 +222,11 @@ export default function Test() {
     console.log(partida.log[turn].events.length)
     console.log(partida.log[turn].events)*/
 
+    if(!partida.log[turn]) {
+      setLog(prevLog => [...prevLog, 'End of battle']);
+      return
+    }
+
     for (let index = 0; index < partida.log[turn].events.length; index++) {
       //console.log(`----- EVENT ${index} -----`)
       let timeout = 0
@@ -125,10 +242,7 @@ export default function Test() {
         setLog(prevLog => [...prevLog, text]);
       }
       if(args[0] === 'move'){
-        //console.log('MOVE')
-        //console.log(args)
         const poke = battle.getPokemon(args[1]);
-        //console.log(poke)
         const move = battle.get("moves", args[2]);
         const poke2 = battle.getPokemon(args[3]);
 
@@ -151,10 +265,69 @@ export default function Test() {
         const detailedPokemon = Protocol.parseDetails(name, positionIdent, pokemonDetails);
     
         console.log('pokemon', pokemon)
+
+         const element = scene.getPosition(pos);
+         if(element){
+          const startY = pos.includes('p1') ? element.y + 40 : element.y - 40;
+
+          const startX = pos.includes('p1') ? element.x -150 : element.x + 150;
+
+          
+          scene.showEffect('pokeball', {
+            opacity: 1,
+            x: startX,
+            y: startY,
+            scale: .7,
+            time: 300 / scene.acceleration,
+          }, {
+            opacity: 0,
+            x: element.x,
+            y: element.y,
+            time: 700 / scene.acceleration,
+          }, 'ballistic2', '', '', () => {
+            setPokemon(prevPokemon => {
+              return {...prevPokemon, [pos]: pokemon};
+            });
+          });  
+
+
+
+         }
+
     
+         /*
         setPokemon(prevPokemon => {
           return {...prevPokemon, [pos]: pokemon};
-        });
+        });*/
+      }
+
+      if(args[0] === 'faint'){
+        const [, positionIdent] = args as [string, PokemonIdent]
+        const pokemon = battle.getPokemon(positionIdent);
+        const pos = positionIdent.split(':')[0];
+        const element = scene.getPosition(pos);
+
+        if(element){
+          const startY = pos.includes('p1') ? element.y + 40 : element.y - 40;
+          const startX = pos.includes('p1') ? element.x -150 : element.x + 150;
+
+          scene.showEffect('pokeball', {
+            opacity: 1,
+            x: element.x,
+            y: element.y,
+            scale: .7,
+            time: 300 / scene.acceleration,
+          }, {
+            opacity: 0,
+            x: startX,
+            y: startY,
+            time: 700 / scene.acceleration,
+          }, 'ballistic2', '', '', () => {
+            setPokemon(prevPokemon => {
+              return {...prevPokemon, [pos]: null};
+            });
+          });  
+        }
       }
     
       battle.currentWeather();
@@ -176,143 +349,32 @@ export default function Test() {
   }
 
 
-  function getPosition(id: string) {
-    const gameElement = document.querySelector('#game');
-
-    const element = document.querySelector(`#${id}`);
-    if (!element || !gameElement) return null;
-
-    const rect = element.getBoundingClientRect();
-    console.log(`Element: ${id} - ${rect.x}, ${rect.y}`);
-   
-
-  }
-
 
   return ( <section className="flex flex-col items-center w-full h-full">
-  ACTIVE:
-  {battle.getAllActive().map(pokemon => {
-    return <div key={pokemon.speciesForme}>{pokemon.speciesForme}</div>
-
-  })}
-  <div className='flex h-[840px] w-[1600px]   justify-evenly border-black  border'
-  
+  <div className='flex  w-[90%]   justify-evenly border-black  border aspect-[16/4] relative'
   //background-image:url(https://play.pokemonshowdown.com/sprites/gen6bgs/bg-darkmeadow.jpg);display:block;opacity:0.8
-  style={{backgroundImage: `url(https://i.imgur.com/H3qR14R.png)`
+  style={{backgroundImage: `url(https://i.imgur.com/EIKbeGK.png)`
   , backgroundSize: 'cover', opacity: 0.8}}
   >
     <div className="w-[20%] flex flex-col  bg-white bg-opacity-40">
       <div>Player 1</div>
-      <PokemonTeam team={partida.team1} />
+      <PokemonTeam team={battle.sides[0].team} />
     </div>
-    <GameCanvas pokemon={pokemon} gameElementRef/>
+    <GameCanvas pokemon={pokemon}/>
     <div className="w-[20%] flex flex-col bg-white bg-opacity-40">
       <div>Player 2</div>
-      <PokemonTeam team={partida.team2} />
+      <PokemonTeam team={battle.sides[1].team} />
     </div>
   </div>
-  <button onClick={() => setTurn(turn + 1)}>Next Turn</button>
-  <button onClick={() => testAttack(battle)}>Test Attack</button>
-  <button onClick={() => test2(battle)}>Test act</button>
-  <div className="h-[30%] overflow-auto">
-    <div>Turn {turn}</div>
-    <div>{log.map((line, index) => (
-      <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
-    ))}</div>
-  </div>
+  <button onClick={() => testAttack(battle)}>Start</button>
+  <div className="h-[30%] overflow-auto" ref={logRef}>
+      <div>{log.map((line, index) => (
+        <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
+      ))}</div>
+    </div>
 </section>
   );
 
 
 }
 
-
-/*
-
-      
-      {Object.keys(partida.log).map((key) => (
-            <div key={key} className='border m-2 flex justify-around items-center'>
-              <img src={partida.log[key].t1.url} alt='pokemon' />
-              
-              <div className='text-black w-[80%] '>
-              <div >Turn {key}</div>
-                {partida.log[key].events.map((line, index) => (
-                  <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
-                ))
-              }</div>
-              <img src={partida.log[key].t2.url} alt='pokemon' />
-            </div>
-          ))}
-
-*/
-
-
-/*
-
-<section className="flex flex-col items-center w-full h-full">
-      ACTIVE:
-      {battle.getAllActive().map(pokemon => {
-        return <div key={pokemon.speciesForme}>{pokemon.speciesForme}</div>
-
-      })}
-      <div className='flex h-[60%] w-[80%]   justify-evenly border-black  border'
-      
-      //background-image:url(https://play.pokemonshowdown.com/sprites/gen6bgs/bg-darkmeadow.jpg);display:block;opacity:0.8
-      style={{backgroundImage: `url(https://play.pokemonshowdown.com/sprites/gen6bgs/bg-darkmeadow.jpg)`, backgroundSize: 'cover', backgroundPosition: 'center', opacity: 0.8}}
-      >
-        <div className="w-[20%] flex flex-col  bg-white bg-opacity-40">
-          <div>Player 1</div>
-          <PokemonTeam team={partida.team1} />
-        </div>
-        <div className="w-[60%] flex flex-col relative">
-          <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[10%]" id="p1a">
-            <div id='hp'>{partida.team1[0].hp}</div>
-            <PokemonImage id={partida.team1[0].speciesForme} side="p1" shiny={false}/>
-          </div>
-
-          {teamSize > 1 && (
-            <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[30%]" id="p1b">
-              <PokemonImage id={partida.team1[1].speciesForme} side="p1" shiny={false}/>
-            </div>
-          )}
-
-          {teamSize > 2 && (
-            <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[50%]" id="p1c">
-              <PokemonImage id={partida.team1[2].speciesForme} side="p1" shiny={false}/>
-            </div>
-          )}
-
-          <div className="w-48 h-48 border border-blue-500 absolute top-16 right-[10%]" id="p2a">
-            <PokemonImage id={partida.team2[0].speciesForme} side="p2" shiny={false}/>
-          </div>
-          
-          {teamSize > 1 && (
-            <div className="w-48 h-48 border border-blue-500 absolute top-0 right-[30%]" id="p2b">
-              <PokemonImage id={partida.team2[1].speciesForme} side="p2" shiny={false}/>
-            </div>
-          )}
-
-          {teamSize > 2 && (
-            <div className="w-48 h-48 border border-blue-500 absolute top-0 right-[50%]" id="p2c">
-              <PokemonImage id={partida.team2[2].speciesForme} side="p2" shiny={false}/>
-            </div>
-          )}
-          
-        </div>
-        <div className="w-[20%] flex flex-col bg-white bg-opacity-40">
-          <div>Player 2</div>
-          <PokemonTeam team={partida.team2} />
-        </div>
-      </div>
-      <button onClick={() => setTurn(turn + 1)}>Next Turn</button>
-      <button onClick={() => testAttack(battle)}>Test Attack</button>
-      <button onClick={() => test2(battle)}>Test act</button>
-      <div>
-        <div>Turn {turn}</div>
-        <div>{partida.log[turn].events.map((line, index) => (
-          <div key={index} dangerouslySetInnerHTML={{ __html: line.text }} />
-        ))}</div>
-      </div>
-    </section>
-
-*/
