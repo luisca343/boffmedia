b80aef19b0939988e5cf711dc0fd8fa86253afa0
Luis Carballo <luisca343@gmail.com>
2024-06-11 18:47:05 +0200

    :sparkles: Move the Pokémon to the center if <3

--- src/app/battlesim/_components/GameCanvas.tsx
+++ src/app/battlesim/_components/GameCanvas.tsx
@@ -1,34 +1,60 @@
 import PokemonSprite from "./PokemonSprite";
 
 export default function GameCanvas({pokemon}: {pokemon: {[key: string]: any}}){
+  const team1 = Object.keys(pokemon).filter(key => key.startsWith('p1'))
+  const team2 = Object.keys(pokemon).filter(key => key.startsWith('p2'))
+  
     return(
         
-    <div id="game" className="w-[60%] flex flex-col relative">
+    <div id="game" className="w-full flex flex-col relative">
       
-    <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[10%]" id="p1a">
-      {pokemon['p1a'] && <PokemonSprite pokemon={pokemon['p1a']} />}
-    </div>
-
-    <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[30%]" id="p1b">
-      {pokemon['p1b'] && <PokemonSprite pokemon={pokemon['p1b']} />}
-    </div>
-  
-    <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[50%]" id="p1c">
-      {pokemon['p1c'] && <PokemonSprite pokemon={pokemon['p1c']} />}
-    </div>
+      {team1.length === 3 ? <>
+        <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[10%]" id="p1a">
+          <PokemonSprite pokemon={pokemon["p1a"]} />
+        </div>
+      
+        <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[30%]" id="p1b">
+          <PokemonSprite pokemon={pokemon["p1b"]} />
+        </div>
+      
+        <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[50%]" id="p1c">
+          <PokemonSprite pokemon={pokemon["p1c"]} />
+        </div>
+        
+        </> : <>
+          <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[30%]" id="p1a">
+          <PokemonSprite pokemon={pokemon["p1a"]} />
+            </div>
+        
+          <div className="w-48 h-48 border border-red-500 absolute bottom-0 left-[50%]" id="p1b">
+            <PokemonSprite pokemon={pokemon["p1b"]} />
+          </div>
+        </>
+      }
  
-    <div className="w-48 h-48 border border-blue-500 absolute top-0 right-[10%]" id="p2a">
-      {pokemon['p2a'] && <PokemonSprite pokemon={pokemon['p2a']} />}
-    </div>
-    
-    <div className="w-48 h-48 border border-blue-500 absolute top-0 right-[30%]" id="p2b">
-      {pokemon['p2b'] && <PokemonSprite pokemon={pokemon['p2b']} />}
-    </div>
-
-    <div className="w-48 h-48 border border-blue-500 absolute top-0 right-[50%]" id="p2c">
-      {pokemon['p2c'] && <PokemonSprite pokemon={pokemon['p2c']} />}
-    </div>
-
+    {team2.length === 3 ? <>
+        <div className="w-48 h-48 border border-red-500 absolute top-0 left-[60%]" id="p2a">
+          <PokemonSprite pokemon={pokemon["p2a"]} />
+        </div>
+      
+        <div className="w-48 h-48 border border-red-500 absolute top-0 left-[40%]" id="p2b">
+          <PokemonSprite pokemon={pokemon["p2b"]} />
+        </div>
+      
+        <div className="w-48 h-48 border border-red-500 absolute top-0 left-[20%]" id="p2c">
+          <PokemonSprite pokemon={pokemon["p2c"]} />
+        </div>
+        
+        </> : <>
+          <div className="w-48 h-48 border border-red-500 absolute top-0 left-[60%]" id="p2a">
+          <PokemonSprite pokemon={pokemon["p2a"]} />
+            </div>
+        
+          <div className="w-48 h-48 border border-red-500 absolute top-0 left-[40%]" id="p2b">
+            <PokemonSprite pokemon={pokemon["p2b"]} />
+          </div>
+        </>
+      }
     
     
   </div>
