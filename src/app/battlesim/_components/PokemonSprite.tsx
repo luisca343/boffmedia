45d2db8ccad2816a7cac6c214c815e2ef4db4242
Luis Carballo <luisca343@gmail.com>
2024-06-11 18:47:31 +0200

    Update PokemonSprite.tsx

--- src/app/battlesim/_components/PokemonSprite.tsx
+++ src/app/battlesim/_components/PokemonSprite.tsx
@@ -6,19 +6,19 @@ export default function PokemonSprite({pokemon} : {pokemon: Pokemon}){
     if(!pokemon) return null
     const {player, position, name } = Protocol.parsePokemonIdent(pokemon.ident)
     const side: "p1" | "p2" | undefined = player === "p1" || player === "p2" ? player : undefined;
-
-
+    
     const hpPercent = Math.floor((pokemon.hp / pokemon.maxhp) * 100)
     const hpColor = hpPercent > 50 ? "bg-green-400" : hpPercent > 20 ? "bg-yellow-400" : "bg-red-400"
 
-
-    return ( <>
-        <div className={`w-full h-8 border border-black rounded-md overflow-hidden`}>
-            <div className={ `h-full ${hpColor}`} style={{width: `${hpPercent}%`}} >
-                <span className="text-white">{pokemon.hp}/{pokemon.maxhp}</span>
+    return ( <div className="h-full w-full flex flex-col items-center ">
+            <div className={`flex w-full h-[20%] border border-black rounded-md overflow-hidden`}>
+                <div className={ `h-full ${hpColor}`} style={{width: `${hpPercent}%`}} >
+                    <span className="text-white">{pokemon.hp}/{pokemon.maxhp}</span>
+                </div>
+            </div>
+            <div className='h-[80%]'>
+                <PokemonImage id={pokemon.speciesForme} side={side} shiny={pokemon.shiny} />
             </div>
         </div>
-        <PokemonImage id={pokemon.speciesForme} side={side} shiny={pokemon.shiny} />
-        </>
     )
 }
