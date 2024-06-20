be00e6ba5e3583c2e69c39adbb8285470f0e56be
Luis Carballo <luisca343@gmail.com>
2024-06-20 12:51:02 +0200

    Disable battle sim, disable rotom not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,15 @@
-"use client"
-import { RotomNotFound } from "@/components/smartrotom/404"
-import { TooltipProvider } from "@/components/ui/tooltip"
-import { usePathname } from "next/navigation"
+
+import { Suspense } from "react"
+
+
 
 export default function NotFound(){
-    const pathname = usePathname()
-    if (pathname.includes('smartrotom')) return <RotomNotFound />
     return (
-        <div className="flex flex-col items-center justify-center h-full">
-            <h1 className="text-5xl font-bold text-shark-500">404</h1>
-            <p className="text-shark-500">Pagina no encontrada.</p>
-        </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="flex flex-col items-center justify-center h-full">
+                <h1 className="text-5xl font-bold text-shark-500">404</h1>
+                <p className="text-shark-500">Pagina no encontrada.</p>
+            </div>
+        </Suspense>
     )
 }
