c875bfde8000593dca1685d0b70f2d2cb3070e92
Luis Carballo <luisca343@gmail.com>
2024-06-24 21:50:36 +0200

    :sparkles: Repositioned elements

--- src/app/battlesim/_components/GameCanvas.tsx
+++ src/app/battlesim/_components/GameCanvas.tsx
@@ -6,27 +6,27 @@ export default function GameCanvas({pokemon}: {pokemon: {[key: string]: any}}){
   
     return(
         
-    <div id="game" className="w-full flex flex-col relative">
+    <div id="game" className="w-full flex flex-col relative border border-red-600">
       
       {team1.length === 3 ? <>
-          <PokemonSprite pokemon={pokemon["p1a"]} id="p1a" className='bottom-0 left-[10%]'/>
-          <PokemonSprite pokemon={pokemon["p1b"]} id="p1b" className='bottom-0 left-[30%]'/>
+          <PokemonSprite pokemon={pokemon["p1a"]} id="p1a" className='bottom-[10%] left-[5%]'/>
+          <PokemonSprite pokemon={pokemon["p1b"]} id="p1b" className='bottom-[5%] left-[25%]'/>
           <PokemonSprite pokemon={pokemon["p1c"]} id="p1c" className='bottom-0 left-[50%]'/>
         </> : <>
-          <PokemonSprite pokemon={pokemon["p1a"]} id="p1a" className='bottom-0 left-[30%]'/>
+          <PokemonSprite pokemon={pokemon["p1a"]} id="p1a" className='bottom-[5%] left-[5%]'/>
           <PokemonSprite pokemon={pokemon["p1b"]} id="p1b" className='bottom-0 left-[50%]'/>
         </>
       }
  
     {team2.length === 3 ? <>
-          <PokemonSprite pokemon={pokemon["p2a"]} id="p2a" className='top-0 left-[60%]'/>
-          <PokemonSprite pokemon={pokemon["p2b"]} id="p2b" className='top-0 left-[40%]'/>
-          <PokemonSprite pokemon={pokemon["p2c"]} id="p2c" className='top-0 left-[20%]'/>
+          <PokemonSprite pokemon={pokemon["p2a"]} id="p2a" className='top-[10%] right-[5%]'/>
+          <PokemonSprite pokemon={pokemon["p2b"]} id="p2b" className='top-[5%] right-[25%]'/>
+          <PokemonSprite pokemon={pokemon["p2c"]} id="p2c" className='top-0 right-[50%]'/>
 
         
         </> : <>
-          <PokemonSprite pokemon={pokemon["p2a"]} id="p2a" className='top-0 left-[60%]'/>
-          <PokemonSprite pokemon={pokemon["p2b"]} id="p2b" className='top-0 left-[40%]'/>
+          <PokemonSprite pokemon={pokemon["p2a"]} id="p2a" className='top-[5%] right-[5%]'/>
+          <PokemonSprite pokemon={pokemon["p2b"]} id="p2b" className='top-0 right-[50%]'/>
         </>
       }
     
