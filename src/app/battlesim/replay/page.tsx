6a703b268b6942a693ba8f757a064f4e9181409e
Luis Carballo <luisca343@gmail.com>
2024-06-25 22:11:12 +0200

    :art: Restructured main window

--- src/app/battlesim/replay/page.tsx
+++ src/app/battlesim/replay/page.tsx
@@ -1,14 +1,14 @@
 // @ts-nocheck
 "use client"
 import { apiGET } from "@/services/boffAPI";
-import { PokemonImage, PokemonTeam } from "../_components/PokemonTeam";
+import { PokemonImage, PokemonTeam, PokemonTeamList } from "../_components/PokemonTeam";
 import { Battle, Pokemon, Side } from "@pkmn/client"
 import { DetailedPokemon, PokemonDetails, PokemonHPStatus, PokemonIdent, Protocol } from "@pkmn/protocol"
 import { useEffect, useRef, useState } from "react";
 import { Dex } from '@pkmn/sim';
 import { Generations } from '@pkmn/data';
 import GameCanvas from "../_components/GameCanvas";
-import { Scene } from "../_components/battle_animations";
+import { PokemonSprite, Scene } from "../_components/battle_animations";
 import { Input } from "@/components/ui/input";
 
 let battle = new Battle(new Generations(Dex as any))
@@ -42,6 +42,9 @@ export default function Test() {
   const [pokemon, setPokemon]  = useState({} as { [position: string]: Pokemon | null })
   const [pokemonUpdates, setPokemonUpdates] = useState([]);
   const [activeMessage, setActiveMessage] = useState('');
+  const [turnInput, setTurnInput] = useState(0);
+  
+
 
   const [log, setLog] = useState([] as any[])
   const [scene, setScene] = useState({} as Scene)
@@ -113,7 +116,6 @@ export default function Test() {
 
   async function testAttack() {
   
-    const delay = ms => new Promise(res => setTimeout(res, ms));
   
     /*
     console.log(`==================== TURN ${turn} ====================`)
@@ -126,119 +128,15 @@ export default function Test() {
     }
 
     for (let index = 0; index < partida.log[turn].events.length; index++) {
-      // check if scene is paused
-      while(scene.paused) {
-        console.log('paused')
-        await delay(100);
-      }
-      //console.log(`----- EVENT ${index} -----`)
-      let timeout = 0
-      const event = partida.log[turn].events[index];
-      const {args, kwArgs, text, line} = event;
-      //console.log(args, kwArgs, text)
-      if(text){
-        playMessage(text);
-      }
-      
-      if(text !=='') {
-        timeout = 100;
-        setLog(prevLog => [...prevLog, text]);
-      }
-      if(args[0] === 'move'){
-        const poke = battle.getPokemon(args[1]);
-        const move = battle.get("moves", args[2]);
-        const poke2 = battle.getPokemon(args[3]);
-
-        
-        const attack = Dex.moves.get(move.id);
-        if(args[3] === ""){
-          args[3] = args[1].includes('p1') ? 'p2a' : 'p1a';
-        }
-
-        await scene.playBattleAnim(move.id, args[1].split(':')[0] as PokemonIdent, args[3].split(':')[0] as PokemonIdent)
-        //await scene.playBattleAnim('flamethrower', args[1].split(':')[0] as PokemonIdent, args[3].split(':')[0] as PokemonIdent)
-        /*
-        switch(attack.category) {
-          case 'Physical':
-            await scene.playBattleAnim('contactattack', args[1].split(':')[0] as PokemonIdent, args[3].split(':')[0] as PokemonIdent)
-            break;
-          default:
-            await scene.playBattleAnim('attack', args[1].split(':')[0] as PokemonIdent, args[3].split(':')[0] as PokemonIdent)
-            break;
-        }*/
-
-      
-        //console.log(poke, move, poke2)
-      }
-      if(args[0] === 'boost'){
-        console.log('boost', args)
-        break;
-      }
-      battle.add(line);
-      if(args[0] === 'switch') {
-        timeout = 1000;
-        const [, positionIdent, pokemonDetails, hpstatus] = args as [string, PokemonIdent, PokemonDetails, PokemonHPStatus]
-        const pos = positionIdent.split(':')[0];
-        const name = pokemonDetails.split(',')[0];
-
-        const pokemon = battle.getPokemon(positionIdent);
-        
-        
-        const detailedPokemon = Protocol.parseDetails(name, positionIdent, pokemonDetails);
-    
-        scene.playEffect('pokeball', positionIdent, () => {
-          setPokemon(prevPokemon => {
-            return {...prevPokemon, [pos]: pokemon};
-          });
-        });
-
-    
-         /*
-        setPokemon(prevPokemon => {
-          return {...prevPokemon, [pos]: pokemon};
-        });*/
-      }
-
-      if(args[0] === 'faint'){
-        const [, positionIdent] = args as [string, PokemonIdent]
-        const pokemon = battle.getPokemon(positionIdent);
-        const pos = positionIdent.split(':')[0];
-        const element = scene.getPosition(pos);
-
-        if(element){
-          const startY = pos.includes('p1') ? element.y + 40 : element.y - 40;
-          const startX = pos.includes('p1') ? element.x -150 : element.x + 150;
-
-          scene.showEffect('pokeball', {
-            opacity: 1,
-            x: element.x,
-            y: element.y,
-            scale: .7,
-            time: 300 / scene.acceleration,
-          }, {
-            opacity: 0,
-            x: startX,
-            y: startY,
-            time: 700 / scene.acceleration,
-          }, 'ballistic2', '', '', () => {
-            setPokemon(prevPokemon => {
-              return {...prevPokemon, [pos]: null};
-            });
-          });  
-        }
-      }
-    
-      battle.currentWeather();
-  
-      await delay(timeout);
+      await performAction(index);
     }
+    
 
     /*
     console.log('==================== END TURN ====================')
     console.log(battle)
     console.log('==================== END UPDATE ====================')
     console.log(battle)*/
-    battle.update();
     setTurn(turn + 1);
   }
 
@@ -247,36 +145,156 @@ export default function Test() {
     scene.playBattleAnim(attack,'p1a' as PokemonIdent, 'p2b' as PokemonIdent);
   }
 
+  function playAudio(url) {
+    return new Promise((resolve, reject) => {
+      const audio = new Audio(url);
+      audio.volume = 0.5;
+      audio.play().then(() => {
+        audio.onended = () => {
+          resolve();
+        };
+      }).catch((error) => {
+        console.error('Error playing audio:', error);
+        reject(error);
+      });
+    });
+  }
+
+  async function performAction(index: number = 0) {
+        const delay = ms => new Promise(res => setTimeout(res, ms));
+          // check if scene is paused
+          while(scene.paused) {
+            console.log('paused')
+            await delay(50);
+          }
+          //console.log(`----- EVENT ${index} -----`)
+          let timeout = 0
+          const event = partida.log[turn].events[index];
+          const {args, kwArgs, text, line} = event;
+          console.log(args, kwArgs, text)
+          if(text){
+            playMessage(text);
+          }
+          
+          if(text !=='') {
+            timeout = 100;
+            setLog(prevLog => [...prevLog, text]);
+          }
+          if(args[0] === 'move'){
+            const poke = battle.getPokemon(args[1]);
+            const move = battle.get("moves", args[2]);
+            const poke2 = battle.getPokemon(args[3]);
+            const attack = Dex.moves.get(move.id);
+            if(args[3] === ""){
+              args[3] = args[1].includes('p1') ? 'p2a' : 'p1a';
+            }
+    
+            await scene.playBattleAnim(move.id, args[1].split(':')[0] as PokemonIdent, args[3].split(':')[0] as PokemonIdent)
+            
+          }
+          if(args[0] === 'boost'){
+            console.log('boost', args)
+            return
+          }
+          battle.add(line);
+          if(args[0] === 'switch') {
+            timeout = 1000 / scene.acceleration;
+            const [, positionIdent, pokemonDetails, hpstatus] = args as [string, PokemonIdent, PokemonDetails, PokemonHPStatus]
+            const pos = positionIdent.split(':')[0];
+            const name = pokemonDetails.split(',')[0];
+    
+            const pokemon = battle.getPokemon(positionIdent);
+            
+            
+            const detailedPokemon = Protocol.parseDetails(name, positionIdent, pokemonDetails);
+        
+            await scene.playEffect('pokeball', positionIdent, async() => {
+              setPokemon(prevPokemon => {
+                return {...prevPokemon, [pos]: pokemon};
+              });
+              const audio = new Audio('/battlesim/audio/cries/mewtwo.mp3');
+              // After setting the Pokemon, play the audio
+              console.log(`https://play.pokemonshowdown.com/audio/cries/${pokemon?.species.id.toLowerCase()}.mp3`)
+              return await playAudio(`https://play.pokemonshowdown.com/audio/cries/${pokemon?.species.id.toLowerCase()}.mp3`);
+            });
+          }
+    
+          if(args[0] === 'faint'){
+            const [, positionIdent] = args as [string, PokemonIdent]
+            const pokemon = battle.getPokemon(positionIdent);
+            const pos = positionIdent.split(':')[0];
+            const element = scene.getPosition(pos);
+    
+            if(element){
+              const startY = pos.includes('p1') ? element.y + 40 : element.y - 40;
+              const startX = pos.includes('p1') ? element.x -150 : element.x + 150;
+
+              await scene.playBattleAnim('faint', pos as PokemonIdent, pos.includes('p1') ? 'p2a' : 'p1a');
+
+
+    
+              await scene.playEffect('pokeball', pos, () => {
+                setPokemon(prevPokemon => {
+                  return {...prevPokemon, [pos]: null};
+                });
+              });
+            }
+          }
+          if(args[0] === '-damage') {
+            timeout = 1000 / scene.acceleration;
+          }
+        
+          battle.currentWeather();
+      
+          battle.update();
+          await delay(timeout);
+  }
+
 
   function changeView() {
     
   }
 
+  function changeTurn() {
+    setTurn(turnInput);
+  }
 
-  return ( <section className="flex flex-col items-center w-full h-full">
-  <div className='flex  justify-evenly border-black  border aspect-[16/4] relative'
-  //background-image:url(https://play.pokemonshowdown.com/sprites/gen6bgs/bg-darkmeadow.jpg);display:block;opacity:0.8
-  style={{backgroundImage: `url(https://i.imgur.com/EIKbeGK.png)`, backgroundSize: 'cover', opacity: 0.8, width:' 800px', height: '450px'}}
-  >
-    <div className="w-[30%] flex flex-col  bg-white bg-opacity-40">
-      <div>Player 1</div>
-      <PokemonTeam team={battle.sides[0].team} />
-    </div>
-    <GameCanvas pokemon={pokemon}/>
-    <div className="w-[30%] flex flex-col bg-white bg-opacity-40">
-      <div>Player 2</div>
-      <PokemonTeam team={battle.sides[1].team} />
-    </div>
+  return ( <section className="flex flex-col  w-full h-full">
+    <div className="flex">
+      <div className='flex  justify-evenly aspect-[16/4] relative'
+      //background-image:url(https://play.pokemonshowdown.com/sprites/gen6bgs/bg-darkmeadow.jpg);display:block;opacity:0.8
+      style={{backgroundImage: `url(https://i.imgur.com/EIKbeGK.png)`, backgroundSize: 'cover', opacity: 0.99, width:' 900px', height: '450px'}}
+      >
+        <div className="w-[30%] flex flex-col  bg-slate-900 bg-opacity-60 justify-end">
+        <PokemonTeamList team={battle.sides[0].team} />
+            <div>{battle.sides[0].name}</div>
+            <img className="mx-auto" style={{height:'100px', width:'50px'}} src="https://crafatar.com/renders/body/67d9b543-5ac9-41e1-a8a5-20d7689e24a4" />
+            
+        </div>
+        <GameCanvas pokemon={pokemon} battle={battle}/>
+        <div className="w-[30%] flex flex-col bg-slate-900 bg-opacity-60">
+        <div>{battle.sides[1].name}</div>
+        <img className="mx-auto" style={{height:'100px', width:'50px', transform: 'scaleX(-1)'}} src="https://crafatar.com/renders/body/e4f3e314-ea7f-4ef6-aa5b-06162c5bf7f6" />
+          
+        <PokemonTeamList team={battle.sides[1].team} />
+        </div>
+      </div>
+
+        <div className="w-[400px] h-[450px] overflow-auto  bg-slate-800 text-slate-100" ref={logRef}>
+          <div>{log.map((line, index) => {
+            if(line.includes('Turn')) return <div key={index} className="font-bold text-2xl  bg-slate-500 text-slate-200"><div key={index} dangerouslySetInnerHTML={{ __html: line }} /></div>
+            return <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
+          })}</div>
+        </div>
   </div>
   <button onClick={() => testAttack(battle)}>Start</button>
   <button onClick={() => test2(battle)}>Test</button>
   <button onClick={() => changeView()}>Change View</button>
+  <button onClick={() => changeTurn()}>Change Turn</button>
+
+  <Input value={turnInput} onChange={(e) => setTurnInput(e.target.value)} />
   <Input value={attack} onChange={(e) => setAttack(e.target.value)} />
-  <div className="h-[30%] overflow-auto" ref={logRef}>
-      <div>{log.map((line, index) => (
-        <div key={index} dangerouslySetInnerHTML={{ __html: line }} />
-      ))}</div>
-    </div>
+  
 </section>
   );
 
