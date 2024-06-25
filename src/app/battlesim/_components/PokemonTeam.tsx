8b2e644b96979df1714fceee907fd56b1c9c126b
Luis Carballo <luisca343@gmail.com>
2024-06-25 22:11:26 +0200

    :sparkles: Test detailed pokemon view

--- src/app/battlesim/_components/PokemonTeam.tsx
+++ src/app/battlesim/_components/PokemonTeam.tsx
@@ -4,12 +4,33 @@ import {
     GraphicsGen, Icons, Sprites
   } from '@pkmn/img';
 import { DetailedPokemon } from "@pkmn/protocol";
+import { HpBar } from "./PokemonSprite";
+
+export function PokemonTeamList({team}: {team: Pokemon[]}) {
+    const teamSize = team.length > 6 ? team.length : 6;
+    return (
+        <div className="flex flex-col justify-center">
+            {Array.from({length: teamSize}, (_, i) => team[i]).map(pokemon => {
+                if(!pokemon) return <div key={Math.random()} className="flex text-white" style={{height:50}}> </div>
+                return <div key={pokemon?.name} className="flex text-white items-center" style={{height:50}}>
+                    <div className="mr-2">
+                        <PokemonSprite key={pokemon?.name} pokemon={pokemon} />
+                    </div>
+                    
+                    <HpBar pokemon={pokemon} />
+                </div>
+        })}
+        </div>
+    )
+}
+
+
 export function PokemonTeam({team}: {team: Pokemon[]}){
     const teamSize = team.length > 6 ? team.length : 6;
     const halfTeamSize = Math.ceil(teamSize / 2);
 
     return (
-        <div>
+        <div  className="mx-auto flex flex-wrap justify-center">
             <>
                 <div>
                     {Array.from({length: halfTeamSize}, (_, i) => team[i]).map(pokemon => (
@@ -26,8 +47,8 @@ export function PokemonTeam({team}: {team: Pokemon[]}){
     )
 }
 
-function PokemonSprite({pokemon}: {pokemon: Pokemon}){
-    if(!pokemon) return <div className=" w-fit inline-block  " style={{width:40, height:30}}><img className="m-auto"  src='/smartrotom/test/pokeball.png' /></div>
+function PokemonSprite({pokemon, className}: {pokemon: Pokemon, className?: string}){
+    if(!pokemon) return <div className=" w-fit inline-block  " style={{width:30, height:30}}><img className="m-auto"  src='/smartrotom/test/pokeball.png' /></div>
     const icon = Icons.getPokemon(pokemon.speciesForme, {
         side: 'p1',
         gender: pokemon.gender || undefined,
@@ -37,7 +58,7 @@ function PokemonSprite({pokemon}: {pokemon: Pokemon}){
 
     return (
         <div 
-        
+            className={className}
             key={pokemon.name}
             style={{...icon.css, width: 30, height: 30}}
         />
