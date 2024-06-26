a6947ddbb71aafeddd59fbd93653b287ec9c0b6a
Luis Carballo <luisca343@gmail.com>
2024-06-26 20:58:58 +0200

    :sparkles: Show data in popup

--- src/app/battlesim/_components/PokemonTeam.tsx
+++ src/app/battlesim/_components/PokemonTeam.tsx
@@ -1,24 +1,58 @@
-"use client"
+
 import { Pokemon } from "@pkmn/client";
 import {
     GraphicsGen, Icons, Sprites
   } from '@pkmn/img';
 import { DetailedPokemon } from "@pkmn/protocol";
 import { HpBar } from "./PokemonSprite";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { Dex } from '@pkmn/sim';
+import TypeBadge, { TypeBadgeSmall } from "@/app/smartrotom/pokedex/entrada/[[...params]]/_components/TypeBadge";
+
+
+function calculateSpeed(base: number, level: number, natureModifier: number, IV: number, EV: number) {
+    const speed = Math.floor((0.01 * (2 * base + IV + Math.floor(0.25 * EV)) * level) + 5) * natureModifier;
+    return speed;
+  }
+
+export function PokemonTeamListElement({pokemon}: {pokemon: Pokemon}) {
+    return (
+        <div className="flex text-white items-center" style={{height:50}}>
+            <div className="mr-2" style={{width:30, height:30}}>
+                <PokemonSprite key={pokemon?.name} pokemon={pokemon} />
+            </div>
+            <HpBar pokemon={pokemon} />
+        </div>
+    )
+}
 
 export function PokemonTeamList({team}: {team: Pokemon[]}) {
     const teamSize = team.length > 6 ? team.length : 6;
     return (
         <div className="flex flex-col justify-center">
             {Array.from({length: teamSize}, (_, i) => team[i]).map(pokemon => {
-                if(!pokemon) return <div key={Math.random()} className="flex text-white" style={{height:50}}> </div>
-                return <div key={pokemon?.name} className="flex text-white items-center" style={{height:50}}>
-                    <div className="mr-2">
+                if(!pokemon) return <div key={Math.random()}  className="flex text-white items-center" style={{height:50}}>
+                    <div className="mr-2" style={{width:30, height:30}}>
                         <PokemonSprite key={pokemon?.name} pokemon={pokemon} />
                     </div>
-                    
                     <HpBar pokemon={pokemon} />
+                
                 </div>
+                return <HoverCard key={pokemon.name} openDelay={0} closeDelay={0}>
+                    <HoverCardTrigger>
+                        <PokemonTeamListElement key={pokemon.name} pokemon={pokemon} />
+                    </HoverCardTrigger>
+                    <HoverCardContent className="z-[200] bg-slate-800 text-slate-100 w-128" side="right">
+                         <span className="font-bold">{pokemon.name}</span> {pokemon.speciesForme} L{pokemon.level}
+                         <div className="flex">{pokemon.types.map(type => <TypeBadgeSmall key={type} type={type} />)}</div>
+                         <br/>
+                         <div>HP: {pokemon.hp > 0 ? pokemon.hp / pokemon.maxhp * 100 : 0}%</div>
+                         <span className="flex">Possible abilities: {pokemon.species.abilities[0]} {pokemon.species.abilities[1] && pokemon.species.abilities[1]} {pokemon.species.abilities.H && pokemon.species.abilities.S}</span>
+                        <span className="flex">Speed: {calculateSpeed(pokemon.species.baseStats.spe, pokemon.level, .9, 0,0 )} - {calculateSpeed(pokemon.species.baseStats.spe, pokemon.level, 1.1, 31, 252)} </span>
+                        <span className="flex">Def: {pokemon.species.baseStats.def} - {pokemon.species.baseStats.def + 31} </span>
+                        {pokemon.movesUsedWhileActive.map(move => <div key={move}>{move}</div>)}
+                    </HoverCardContent>
+                </HoverCard>
         })}
         </div>
     )
@@ -48,7 +82,10 @@ export function PokemonTeam({team}: {team: Pokemon[]}){
 }
 
 function PokemonSprite({pokemon, className}: {pokemon: Pokemon, className?: string}){
-    if(!pokemon) return <div className=" w-fit inline-block  " style={{width:30, height:30}}><img className="m-auto"  src='/smartrotom/test/pokeball.png' /></div>
+    if(!pokemon) return <div 
+    style={{width:40, height:40}}>
+        <img className="m-auto"  src='/smartrotom/test/pokeball.png' />
+    </div>
     const icon = Icons.getPokemon(pokemon.speciesForme, {
         side: 'p1',
         gender: pokemon.gender || undefined,
