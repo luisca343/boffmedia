2bebcc7562b193916e542b9d6e4096d6c1f38d4a
Luis Carballo <luisca343@gmail.com>
2024-07-13 22:58:18 +0200

    :sparkles: Implement PossibleSpawnSection

--- src/app/smartrotom/pokedex/localizacion/[id]/page.tsx
+++ src/app/smartrotom/pokedex/localizacion/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { rotomGET } from "@/services/boffAPI";
 import { PokemonSprite } from "../../_components/PokemonSprite";
 import useTranslation from 'next-translate/useTranslation'
-import { PossibleSpawns } from "../../_components/PossibleSpawns";
+import { PossibleSpawns, PossibleSpawnsSection } from "../../_components/PossibleSpawns";
 import { useEffect, useState } from "react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
@@ -30,16 +30,15 @@ export default function Localizacion({params} : {params: {id: string}}){
     return(
         <div className="bg-main-800  ">
             <div className="hidden 2xl:flex justify-center">
-                <Label className="text-white text-2xl">Mostrar Atrapados</Label>
-                <Checkbox id="caught" onClick={() => setHideCaught(!hideCaught)} className="bg-main-400 text-white p-2 rounded-xl m-2">Mostrar Atrapados</Checkbox>
                 <Label className="text-white text-2xl">Mostrar Avistados</Label>
                 <Checkbox id="seen" onClick={() => setHideSeen(!hideSeen)} className="bg-main-400 text-white p-2 rounded-xl m-2">Mostrar Avistados</Checkbox>
+                <Label className="text-white text-2xl">Mostrar Atrapados</Label>
+                <Checkbox id="caught" onClick={() => setHideCaught(!hideCaught)} className="bg-main-400 text-white p-2 rounded-xl m-2">Mostrar Atrapados</Checkbox>
             </div>
             <div className="flex  flex-wrap justify-center items-start">
                 {Object.entries(pokemon).map(([biome, spawn]) => (
                     <div key={1} className="flex flex-col  mb-4 rounded-xl p-2 w-[95%] m-auto">
-                        <h1 className="text-4xl text-center text-main-100 font-bold">{spawnsTranslation(biome.replace(":", "_").replace("%3A","_").replace("%20","_"))}</h1>
-                        <PossibleSpawns pokemonSpawns={spawn} hideCaught={hideCaught} hideSeen={hideSeen}/>
+                        <PossibleSpawnsSection pokemonSpawns={spawn} hideCaught={hideCaught} hideSeen={hideSeen} title={spawnsTranslation(biome.replace(":", "_").replace("%3A","_").replace("%20","_"))}/>
                     </div>
                 ))}
             </div>
