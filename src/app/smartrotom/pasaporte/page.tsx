6a10bee1f67fb282f9d83fa032929623f69f0c74
Luis Carballo <luisca343@gmail.com>
2024-07-25 21:26:03 +0200

    :sparkles: Added numbers to pages

--- src/app/smartrotom/pasaporte/page.tsx
+++ src/app/smartrotom/pasaporte/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Book, Page } from "@/components/ui/book/book";
+import { Book, Page, PageFlip, turnPage } from "@/components/ui/book/book";
 import './pasaporte.css'
 import { useSession } from "next-auth/react";
 import { BoffSession } from "@/components/smartrotom/AppWrapper";
@@ -8,9 +8,10 @@ import { useEffect, useState } from "react";
 import { rotomPOST } from "@/services/boffAPI";
 import ActiveTeam from "./_components/ActiveTeam";
 import useTranslation from 'next-translate/useTranslation'
+import Badges from "./_components/Badges";
 
 export default function Pasaporte(){
-  const [book, setBook] = useState(null) as any
+  const [book, setBook] = useState(null as unknown as PageFlip) as [PageFlip, any]
   const {data: session} = useSession() as {data: BoffSession | null, status: string};
   const uuid = session?.user.smartRotomUser.uuid as string
   const username = session?.user.smartRotomUser.username as string
@@ -18,6 +19,7 @@ export default function Pasaporte(){
 
   const [stats, setStats] = useState(null) as any
   const [team,setTeam] = useState(null) as any
+  const [ achievements, setAchievements] = useState(null) as any
 
   useEffect(()=>{
     console.log('uuid',uuid)
@@ -28,21 +30,31 @@ export default function Pasaporte(){
       console.log('team',res)
       setTeam(res)
     })
+    rotomPOST('/achievements',{uuid}).then((res)=>{
+      console.log('achievements',res)
+      setAchievements(res)
+    })
   },[])
 
   let page  = 0;
     return(
       <section className=" bg-yellow-200 flex bg-center bg-no-repeat bg-fixed bg-cover">
           <Book setBook={setBook}>
-            <Page className="font-vinque bg-blue-600 flex  flex-col " style={{backgroundImage: `url(/smartrotom/img/apps/pasaporte/cuero.webp)`}}>
+            <Page book={book} number={0} className="font-vinque bg-blue-600 flex  flex-col " style={{backgroundImage: `url(/smartrotom/img/apps/pasaporte/cuero.webp)`}}>
               <div className="text-center text-6xl mt-4 text-yellow-200 font-bold opacity-80" style={{ mixBlendMode: 'normal' }}>PASAPORTE</div>
               <img className="h-0 flex-1  opacity-80" src="/smartrotom/img/logo.webp" alt="description" style={{ mixBlendMode: 'normal' }} />
               <div className="mb-4 text-center text-4xl  text-yellow-200 font-bold  opacity-80" style={{ mixBlendMode: 'normal' }}>Región de Teras</div>
             </Page>
-            <Page>
+            <Page book={book} number={1} >
               <PageTitle title="Indice"/>
+              <div className="flex flex-col">
+                <button onClick={(e) => turnPage(book, 2, e)} className="text-2xl font-bold">1. Datos Jugador</button>
+                <button onClick={(e) => turnPage(book, 3, e)} className="text-2xl font-bold">2. Equipo Actual</button>
+                <button onClick={(e) => turnPage(book, 4, e)} className="text-2xl font-bold">3. Medallas</button>
+
+              </div>
             </Page>
-            <Page>
+            <Page book={book} number={2} >
               <div className="flex flex-col">
                 <PageTitle title="Datos Jugador"/>
                 <div className="flex">
@@ -55,14 +67,17 @@ export default function Pasaporte(){
                
               </div>
             </Page>
-            <Page>
+            <Page book={book} number={3} >
               <PageTitle title="Equipo Actual"/>
               {team && <ActiveTeam team={team} />}
             </Page>
-            <Page>Page 4</Page>
-            <Page>Page 5</Page>
-            <Page>Page 6</Page>
-            <Page style={{backgroundImage: `url(/smartrotom/img/apps/pasaporte/cuero.webp)`}}>
+            <Page book={book} number={4} >
+              <PageTitle title="Medallas"/>
+              <Badges achievementData={achievements}></Badges>
+            </Page>
+            <Page book={book} number={5} >Page 5</Page>
+            <Page book={book} number={6} >Page 6</Page>
+            <Page book={book} number={7}  style={{backgroundImage: `url(/smartrotom/img/apps/pasaporte/cuero.webp)`}}>
             </Page>
           </Book>
       </section>
