ff6c18ff455c10c6a7ecb9b63f6e9429091a6f2f
Luis Carballo <luisca343@gmail.com>
2024-07-26 19:11:26 +0200

    :art: Adjusted sizes to handheld mode

--- src/app/smartrotom/pasaporte/_components/Badges.tsx
+++ src/app/smartrotom/pasaporte/_components/Badges.tsx
@@ -1,18 +1,7 @@
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+import { parseDate } from "@/lib/utils";
+import { SmartRotomAchievement } from "../types";
 
-interface SmartRotomAchievement {
-    id: string;
-    name: string;
-    description: string;
-    icon: string;
-    category: string;
-    subcategory: string;
-    target: number;
-    progress: number;
-    completed: boolean;
-    completedAt: Date;
-    uuid: string;
-}
 
 export default function Badges({achievementData}: {achievementData: SmartRotomAchievement[]}){
     if(!achievementData) return null
@@ -20,36 +9,44 @@ export default function Badges({achievementData}: {achievementData: SmartRotomAc
     const fukitsuGansolia = achievementData.filter((a)=>a.subcategory === 'Fukitsu-Gansolia')
     const narukamiAkina = achievementData.filter((a)=>a.subcategory === 'Narukami-Akina')
     const ligas = achievementData.filter((a)=>a.category === 'Ligas')
+    const frenteBatalla = achievementData.filter((a)=>a.category === 'Frente Batalla')
 
     const completedPrincipiantes = principiantes.filter((a)=>a.completed)
     const completedFukitsuGansolia = fukitsuGansolia.filter((a)=>a.completed)
     const completedNarukamiAkina = narukamiAkina.filter((a)=>a.completed)
     const completedLigas = ligas.filter((a)=>a.completed)
+    const completedFrenteBatalla = frenteBatalla.filter((a)=>a.completed)
 
     return(
         <div className="flex flex-col p-2">
-            <div className="flex flex-col mb-4">
-                <span className="font-bold text-2xl mb-2"> Circuito de Principiantes [{completedPrincipiantes.length}/{principiantes.length}]</span>
+            <div className="flex flex-col mb-4 2xl:mb-8">
+                <span className="font-bold text-2xl mb-4"> Circuito de Principiantes [{completedPrincipiantes.length}/{principiantes.length}]</span>
                 <div className="flex flex-row">
                     {principiantes.map((a)=><Badge key={a.id} data={a} />)}
                 </div>
             </div>
-            <div className="flex flex-col mb-4">
-                <span className="font-bold text-2xl mb-2">Circuito de Fukitsu-Gansolia [{completedFukitsuGansolia.length}/{fukitsuGansolia.length}]</span>
+            <div className="flex flex-col mb-4 2xl:mb-8">
+                <span className="font-bold text-2xl mb-4">Circuito de Fukitsu-Gansolia [{completedFukitsuGansolia.length}/{fukitsuGansolia.length}]</span>
                 <div className="flex flex-row">
                     {fukitsuGansolia.map((a)=><Badge key={a.id} data={a} />)}
                 </div>
             </div>
-            <div className="flex flex-col mb-4">
-                <span className="font-bold text-2xl mb-2">Circuito de  Narukami-Akina [{completedNarukamiAkina.length}/{narukamiAkina.length}]</span>
+            <div className="flex flex-col mb-4 2xl:mb-8">
+                <span className="font-bold text-2xl mb-4">Circuito de  Narukami-Akina [{completedNarukamiAkina.length}/{narukamiAkina.length}]</span>
                 <div className="flex flex-row">
                     {narukamiAkina.map((a)=><Badge key={a.id} data={a} />)}
                 </div>
             </div>
-            <div className="flex flex-col mb-4">
-                <span className="font-bold text-2xl mb-2">Ligas [{completedLigas.length}/{ligas.length}]</span>
+            <div className="flex flex-col mb-4 2xl:mb-8">
+                <span className="font-bold text-2xl mb-4">Ligas [{completedLigas.length}/{ligas.length}]</span>
                 <div className="flex flex-row">
                     {ligas.map((a)=><Badge key={a.id} data={a} />)}
+                </div>
+            </div>
+            <div className="flex flex-col mb-4 2xl:mb-8">
+                <span className="font-bold text-2xl mb-4">Frente Batalla [{completedFrenteBatalla.length}/{frenteBatalla.length}]</span>
+                <div className="flex flex-row">
+                    {frenteBatalla.map((a)=><Badge key={a.id} data={a} />)}
                 </div>
             </div>
             
@@ -63,13 +60,14 @@ function Badge({data}: {data: SmartRotomAchievement}){
         <HoverCard >
             <HoverCardTrigger className="flex flex-row w-full justify-around">
                 <img src={`https://api.boffmedia.es/smartrotom/img/logros/${data.icon}.webp`} alt={data.name} 
-                        className={`h-12 w-12 ${data.completed ? '' : 'filter brightness-0'}`}
+                        className={`w-12 h-12 2xl:h-16 2xl:w-16 ${data.completed ? '' : 'filter brightness-0'}`}
                     />
             </HoverCardTrigger>
             <HoverCardContent variant="paper" align="start" className="absolute z-50">
                 <div className="flex flex-col">
                     <span className="font-bold text-xl">{data.name}</span>
                     <span>{data.description}</span>
+                    <span>{data.completed ? `Obtenida: ${parseDate(data.completedAt)}` : 'No completado'}</span>
                 </div>
             </HoverCardContent>
         </HoverCard>
