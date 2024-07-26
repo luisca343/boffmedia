2c84cf5c31dad444e4bef7dab78538dbdf297acf
Luis Carballo <luisca343@gmail.com>
2024-07-26 19:12:10 +0200

    :sparkles: Extracted types

---
+++ src/app/smartrotom/pasaporte/types.ts
@@ -0,0 +1,41 @@
+import { SmartRotomPost } from "@/types";
+import { ActivePokemon } from "@/types/Pokemon";
+
+interface AchievementData {
+    replay: string
+    team: ActivePokemon[]
+}
+
+
+export interface LogroCombate extends SmartRotomPost {
+    npc: string;
+    victoria: boolean;
+    logro: string;
+    equipo: ActivePokemon[];
+    replay: string;
+  }
+
+  export interface SmartRotomAchievement {
+    id: string;
+    name: string;
+    description: string;
+    icon: string;
+    category: string;
+    subcategory: string;
+    target: number;
+    progress: number;
+    completed: boolean;
+    completedAt: Date;
+    uuid: string;
+    data: string;
+}
+
+export function parseAchievementData(data: string): AchievementData {
+    try {
+        console.log("PARSING ACHIEVEMENT DATA", JSON.parse(data));
+        return JSON.parse(data);
+    } catch (e) {
+        console.error('Error parsing achievement data', e);
+        return {} as AchievementData;
+    }
+}
