d665644445dab4e24b08e1aa7b6fe909de291451
Luis Carballo <luisca343@gmail.com>
2024-07-29 12:50:18 +0200

    :sparkles: Use style instead of class for dynamic offsets

--- src/app/battlesim/_components/BattleCanvas.tsx
+++ src/app/battlesim/_components/BattleCanvas.tsx
@@ -17,14 +17,14 @@ export function BattleCanvas({battle}: {battle: Battle}){
     }
     
     return (
-        <div id="game"  className="w-[440px] h-[360px] border border-red-500 relative">
-            <div className="absolute top-1 left-1 bg-slate-800 py-1 px-2 rounded-md text-slate-200 border border-slate-200">Turn {battle.turn}</div>
+        <div id="game"  className="w-[440px] h-[360px] relative">
+            <div className="absolute top-1 left-1 bg-slate-800 py-1 px-2 rounded-md text-slate-200 ">Turn {battle.turn}</div>
         
-            <PokemonElement pokemon={pokemon["p1a"]} id="p1a" className={`top-[${offsets.p1a.top}px] left-[${offsets.p1a.left}px]`}/>
-            <PokemonElement pokemon={pokemon["p1b"]} id="p1b" className={`top-[${offsets.p1b.top}px] left-[${offsets.p1b.left}px]`}/>
+            <PokemonElement pokemon={pokemon["p1a"]} id="p1a" style={{top: offsets.p1a.top, left: offsets.p1a.left}}/>
+            <PokemonElement pokemon={pokemon["p1b"]} id="p1b" style={{top: offsets.p1b.top, left: offsets.p1b.left}}/>
 
-            <PokemonElement pokemon={pokemon["p2b"]} id="p2b" className={`top-[${offsets.p2b.top}px] left-[${offsets.p2b.left}px]`}/>
-            <PokemonElement pokemon={pokemon["p2a"]} id="p2a" className={`top-[${offsets.p2a.top}px] left-[${offsets.p2a.left}px]`}/>
+            <PokemonElement pokemon={pokemon["p2b"]} id="p2b" style={{top: offsets.p2b.top, left: offsets.p2b.left}}/>
+            <PokemonElement pokemon={pokemon["p2a"]} id="p2a" style={{top: offsets.p2a.top, left: offsets.p2a.left}}/>
         </div>
     )
 }
