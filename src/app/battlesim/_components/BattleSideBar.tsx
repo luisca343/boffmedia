746e6c56f2811a620a8605b7f40431f19eac3bba
Luis Carballo <luisca343@gmail.com>
2024-07-31 09:50:47 +0200

    Load  minecraft skin if not a showdown battle

--- src/app/battlesim/_components/BattleSideBar.tsx
+++ src/app/battlesim/_components/BattleSideBar.tsx
@@ -12,20 +12,23 @@ import { PokemonSprite } from "./PokemonSprite";
   export function PlayerDataBar({ battle, side, pov } : { battle: Battle, side: 'p1' | 'p2', pov: 'p1' | 'p2' }) {
     const player = battle[side];
   
-    const avatarId = player?.avatar || 0;
+    const avatarId = player?.avatar || 'unknown';
     const avatar = Sprites.getAvatar(avatarId)
     
     //console.log(player.team)
-
+    console.log(player)
+    console.log('avatarId', avatarId)
+    console.log('avatar', avatar)
+    
     return (
       <div className="border border-black w-32">
         <div>{player.name}</div>
         {
-            avatarId != 0 ? <img src={avatar} alt="avatar"/>
+            avatarId != 'unknown' ? <img src={avatar} alt="avatar"/>
             : <img className="mx-auto" alt="avatar"
             style={{
                 height:'100px', width:'45px', 
-                transform: side === 'p1' ? 'scaleX(-1)' : undefined}
+                transform: side === 'p2' ? 'scaleX(-1)' : undefined}
             } 
             src="https://crafatar.com/renders/body/67d9b543-5ac9-41e1-a8a5-20d7689e24a4"  
             />
@@ -40,16 +43,24 @@ function PokemonTeam({team}: {team: Pokemon[]}){
     const teamSize = team.length > 6 ? team.length : 6;
     const halfTeamSize = Math.ceil(teamSize / 2);
 
+    console.log(team)
+    // Remove duplicates, don't know why they are there.
+    const team1 = team.filter((pokemon, index, self) =>
+        index === self.findIndex((t) => (
+            t.searchid === pokemon.searchid
+        ))
+    )
+
 
     return (
         <div>
           <div className="flex flex-row justify-around">
-            {Array.from({ length: halfTeamSize }, (_, i) => team[i]).map(pokemon => (
+            {Array.from({ length: halfTeamSize }, (_, i) => team1[i]).map(pokemon => (
               <PokemonSprite key={crypto.randomUUID()} pokemon={pokemon} />
             ))}
           </div>
           <div className="flex flex-row justify-around">
-            {Array.from({ length: teamSize - halfTeamSize }, (_, i) => team[i + halfTeamSize]).map(pokemon => (
+            {Array.from({ length: teamSize - halfTeamSize }, (_, i) => team1[i + halfTeamSize]).map(pokemon => (
               <PokemonSprite key={crypto.randomUUID()} pokemon={pokemon} />
             ))}
           </div>
