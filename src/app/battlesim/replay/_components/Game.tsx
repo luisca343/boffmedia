9d48561abfcd7b278cba7f800f04147811a60b56
Luis Carballo <luisca343@gmail.com>
2024-08-04 22:44:39 +0200

    :sparkles: Reworking Battle System for better navigation

--- src/app/battlesim/replay/_components/Game.tsx
+++ src/app/battlesim/replay/_components/Game.tsx
@@ -6,19 +6,25 @@ import { Dex } from '@pkmn/sim';
 import { create } from "zustand";
 import { ArgType, BattleArgsKWArgType, PokemonDetails, PokemonHPStatus, PokemonIdent, Protocol } from "@pkmn/protocol";
 import { LogFormatter } from '@pkmn/view';
-import { PlayerDataBar } from "../../_components/BattleSideBar";
 import { BattleCanvas } from "../../_components/BattleCanvas";
 import { Scene } from "../../_components/Scene";
 import { Button } from "@/components/ui/button";
-import { rotomGET } from "@/services/boffAPI";
-import { time } from "console";
+import { Input } from "@/components/ui/input";
+import { set } from "react-hook-form";
 
 export function Game({battleName = 'medalla_doku'}: {battleName?: string}) {
   const [battleLog, setBattleLog] = useState<string | null>(null);
-  const [actionQueue, setActionQueue] = useState<string[]>([]);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const [currentAction, setCurrentAction] = useState<number>(0);
   const [scene, setScene] = useState<Scene | null>(null);
   const [htmlLog, setLog] = useState<string[]>( []);  
+  
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const [turnInput, setTurnInput] = useState<number>(0);
+  const [settingTurn, setSettingTurn] = useState(false);
+  const [lastTurn, setLastTurn] = useState<number>(0);
+
   
   const logRef = useRef<HTMLDivElement>(null);
 
@@ -29,22 +35,18 @@ export function Game({battleName = 'medalla_doku'}: {battleName?: string}) {
   }, [htmlLog]);
 
   const [pov, setPov] = useState<'p1' | 'p2'>('p1');
+  
   const battle = useBattle() as Battle;
   const setBattle = useSetBattle();
 
   const formatter = new LogFormatter('p1', battle);
-  
 
   useEffect(() => {
-    
     /*rotomGET(`/achievement/67d9b543-5ac9-41e1-a8a5-20d7689e24a4/${battleName}`)
       .then((res) => {
         const replay = res.replay;
-        console.log(replay);
-
         setBattleLog(replay);
         const gameElement = document.querySelector('#game') as HTMLElement;
-        //console.log('gameElement', gameElement)
         const battleScene = new Scene(battle, gameElement);
         setScene(battleScene);
     })
@@ -55,244 +57,155 @@ export function Game({battleName = 'medalla_doku'}: {battleName?: string}) {
       .then(response => response.text())
       .then(text => {
         setBattleLog(text);
-        
-        const gameElement = document.querySelector('#game') as HTMLElement;
-        //console.log('gameElement', gameElement)
-        const battleScene = new Scene(battle, gameElement);
-        setScene(battleScene);
       })
       .catch(error => console.error("Error fetching battle log:", error));
   }, []);
 
+  function loadGameData(){
+    const lines = battleLog ? battleLog.split('\n') : [];
+    let started = false;
+    let finalTurn = 0;
+    for (const line of lines) {
+      if(line.includes('|start')) started = true;
+      if(!started) battle.add(line);
+      if(line.includes('|turn|')) finalTurn++;
+
+    }
+
+    setLastTurn(finalTurn);
+
+    const gameElement = document.querySelector('#game') as HTMLElement;
+    const battleScene = new Scene(battle, gameElement);
+
+    setScene(battleScene);
+    setCurrentAction(0);
+  }
 
   useEffect(() => {
-    if(!isPlaying && actionQueue.length > 0){
-      setIsPlaying(true);
-      const action = actionQueue[0];
-
-      if(action.startsWith('AUDIO:') || action.startsWith('AUDIO_FAINT:')) {
-        let audio
-        if(action.startsWith('AUDIO_FAINT:')) {
-          // If faint, lower the pitch
-          audio = new Audio(action.split('AUDIO_FAINT:')[1]);
-          audio.playbackRate = 0.7;
-        } else {
-          audio = new Audio(action.split('AUDIO:')[1]);
-        }
-          
-        audio.play();
-        setActionQueue(actionQueue.slice(2));
-        setTimeout(() => setIsPlaying(false), 500);
-        return;
-      }
-
-      performAction(copyBattle(battle), action);
-      setActionQueue(actionQueue.slice(1));
-    }
-  }, [isPlaying, actionQueue, battle]);
-  
-  function readTurn(localBattle: Battle, log: string, targetTurn: number) {
-    if(targetTurn < 0) targetTurn = 0;
-
-    if(!localBattle || targetTurn === 0 || localBattle.turn > targetTurn) {
-      localBattle = new Battle(new Generations(Dex as any));
+    if(!dataLoaded && battleLog){
+      loadGameData();
     }
 
-    //console.log('CURRENT BATTLE TURN:', localBattle.turn);
-    let turnQueue: string[] = [];
-    return new Promise<void>((resolve, reject) => {
-      try {
-        const lines = log.split("\n");
-        let logTurn = 0;
-        for (const line of lines) {
-          const { args, kwArgs } = Protocol.parseBattleLine(line);
-  
-          if (args[0] === 'turn') {
-            const turnNum = parseInt(args[1]);
-            logTurn = turnNum;
-            localBattle.setTurn(targetTurn);
-          } else if (args[0] === 'win') {
-            // Handle win condition
-          }
-
-          if(localBattle.turn > logTurn){
-            // Skip turn
-            //console.log('Skipping turn:', logTurn);
-            continue;
-          }
-
-          if (logTurn > targetTurn) {
-            //console.log('Breaking at turn:', logTurn);
-            break;
-          }
-          
-          // Add all lines up to the specified turn
-          if (logTurn <= targetTurn) {
-            if(logTurn === targetTurn){
-              turnQueue.push(line);
-            } else {
-              localBattle.add(args, kwArgs);
-            }
-
-            //console.log('Adding line:', logTurn);
-          }
-        }
-        setBattle(localBattle);
-        setActionQueue(turnQueue);
-        setIsPlaying(false);
-
-        resolve();
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
-
-  let timeout = {start: 0, end: 0};
-  const delay = (ms: number | undefined) => new Promise(res => setTimeout(res, ms));
-
-  function resetTimeout() {
-    timeout = {start: 0, end: 0};
-  }
-  
-
-  async function performAction(localBattle: Battle, line: string) {
-    if(!scene) return;
-    const {args, kwArgs} = Protocol.parseBattleLine(line);
-
-
-    if(timeout.start > 0){
-      await delay(timeout.start).then(() => {
-        resetTimeout();
-        performAction(localBattle, line);
-      });
-
-      return;
-    }
-
-    
-    
-    if(args[0] === 'switch') {
-      timeout.end = 1000 / scene.acceleration;
-      const [, positionIdent, pokemonDetails, hpstatus] = args as [string, PokemonIdent, PokemonDetails, PokemonHPStatus]
-      const pos = positionIdent.split(':')[0];
-      const name = pokemonDetails.split(',')[0];
-      
-      //console.log('switching', pos, name)
-      const switchedInPokemon = localBattle.getSwitchedPokemon(positionIdent, pokemonDetails);
-      const switchedOutPokemon = localBattle.getSwitchedOutPokemon(positionIdent, pokemonDetails);
-
-      if(!switchedOutPokemon){
-        await scene.playEffect('pokeball', positionIdent, async() => {
-          setBattle(localBattle);
-          const audioUrl = `AUDIO:https://play.pokemonshowdown.com/audio/cries/${switchedInPokemon?.species?.id.toLowerCase()}.mp3`
-          setActionQueue([audioUrl, ...actionQueue]);
-          
-
-        });
-      } else {
-        await scene.playEffect('switch', positionIdent, async() => {
-          setBattle(localBattle);
-          const audioUrl = `AUDIO:https://play.pokemonshowdown.com/audio/cries/${switchedInPokemon?.species?.id.toLowerCase()}.mp3`
-          setActionQueue([audioUrl, ...actionQueue]);
-        });
-      }
-    }
-
-    if(args[0] === 'faint'){
-      const [, positionIdent] = args as [string, PokemonIdent];
-      timeout.end = 1000 / scene.acceleration;
-      //console.log('fainting', positionIdent)
-      const pokemon = localBattle.getPokemon(positionIdent);
-      if(pokemon){
-        await scene.playBattleAnim('faint', args[1].split(':')[0] as PokemonIdent, args[1].split(':')[0] as PokemonIdent, async() => {
-          setBattle(localBattle);
-          const audioUrl = `AUDIO_FAINT:https://play.pokemonshowdown.com/audio/cries/${pokemon.species.id.toLowerCase()}.mp3`
-          setActionQueue([audioUrl, ...actionQueue]);
-        });
-      }
-
-      
-
-    }
-
-    if(args[0] === 'move'){
-      if(!line.includes('[still]')){
-        const move = localBattle.get("moves", args[2]);
-        
-        let arg3 = args[3] === "" ? args[1].includes('p1') ? 'p2a' : 'p1a' : args[3] as string
-
-        scene.playBattleAnim(move.id, args[1].split(':')[0] as PokemonIdent, arg3.split(':')[0] as PokemonIdent, async() => {
-          endAction(args, kwArgs, localBattle);
-        });
-        timeout.start = 10000 / scene.acceleration;
-        timeout.end = -1
-      } else {
-        timeout.end = 1000 / scene.acceleration;
-        
-        endAction(args, kwArgs, localBattle);
-      }
-    }
-    
-    const logLine = formatter.formatHTML(args, kwArgs);
-    setLog([...htmlLog, logLine]);
-    if(timeout.end >= 0) await delay(timeout.end).then(() => {
-      endAction(args, kwArgs, localBattle);
-    });
-  }
-
-  function endAction(args: ArgType, kwArgs: BattleArgsKWArgType, localBattle: Battle) {
-    localBattle.add(args, kwArgs);
-    setBattle(localBattle);
-    setIsPlaying(false);
-
-  }
-
-
-  useEffect(() => {
-    if (battleLog) {
-      readTurn(copyBattle(battle), battleLog, 0).then(() => {
-        
-      }).catch((error) => {
-        console.error('Error reading log:', error);
-      });
-    }
   }, [battleLog]);
 
   /*
-  useEffect(() => {
-    console.log('Reading Log');
-    if (battleLog) {
-      readLog(battleLog).then(() => {
-        
-      }).catch((error) => {
-        console.error('Error reading log:', error);
-      });
+useEffect(() => {
+  if (isPlaying && dataLoaded) {
+    const lines = battleLog ? battleLog.split('\n') : [];
+    const actions = lines.slice(battle.turn);
+    let startFound = false;
+
+    const playActionsSequentially = async () => {
+      for (const line of actions) {
+        if (line.includes('|start')) startFound = true;
+        await playAction(line);
+      }
+    };
+
+    playActionsSequentially();
+  }
+}, [isPlaying, dataLoaded]);*/
+
+useEffect(() => {
+  console.log('Current action', currentAction);
+  console.log('Is playing', isPlaying);
+  if( currentAction === -1){
+    console.log('Setting turn');
+    const lines = battleLog ? battleLog.split('\n') : [];
+    const currBattle = new Battle(new Generations(Dex as any));
+    let newTurn = turnInput;
+
+    if(turnInput === 0) newTurn = 1;
+    if(turnInput > lastTurn) newTurn = lastTurn;
+    setTurnInput(newTurn);
+
+    setLog([]);
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      const {args, kwArgs} = Protocol.parseBattleLine(line);
+      if (args[0] === 'turn') {
+        const currentTurn = parseInt(args[1]);
+        if (currentTurn === newTurn) {
+          setBattle(copyBattle(currBattle));
+          setCurrentAction(i);
+          setSettingTurn(false);
+          break;
+        }
+      }
+      currBattle.add(line);
+      setLog((prev) => [...prev, formatter.formatHTML(args, kwArgs)]);
     }
-  }, [battleLog]);*/
+    return;
+  }
 
-  useEffect(() => {
-    //console.log(battle);
-  }, [battle]);
 
+  if(isPlaying) {
+    const lines = battleLog ? battleLog.split('\n') : [];
+    if(lines.length === 0 || currentAction >= lines.length) {
+      setIsPlaying(false);
+      return;
+    }
+    const action = lines[currentAction];
+    playAction(action);
+  }
+}, [currentAction, isPlaying]);
+
+function setCurrentTurn() {
+  if(isPlaying) {
+    setSettingTurn(true);
+  } else {
+    setCurrentAction(-1);
+  }
+}
+
+  async function playAction(line: string) {
+    const { args, kwArgs } = Protocol.parseBattleLine(line);
+    console.log(args, kwArgs);
   
-  async function simulateFaint() {
-    if(!scene) return;
-    await scene.playBattleAnim('flamethrower', 'p1a' as PokemonIdent, 'p2a' as PokemonIdent, async() => {
-        console.log('fainted')
+    // We wait for the animation to finish
+    return new Promise<void>(async (resolve) => {
+        const html = formatter.formatHTML(args, kwArgs);
+        setLog((prev) => [...prev, html]);
+
+        await performAction(args, kwArgs);
+  
+        battle.add(line);
+        setBattle(copyBattle(battle));
+        resolve();
     });
   }
 
-  async function simulateOtherAttack() {
-    if(!scene) return;
-    await scene.playBattleAnim('flamethrower', 'p2b' as PokemonIdent, 'p1b' as PokemonIdent, async() => {
-      console.log('attacked')
+  async function performAction(args: ArgType | BattleArgsKWArgType[], kwArgs: BattleArgsKWArgType) {
+    switch (args[0]) {
+      case 'switch':
+        await switchPokemon(args[1] as PokemonIdent, args[2] as PokemonDetails, args[3] as PokemonHPStatus);
+        break;
+    }
+    let timeout = 500
+    return await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        let nextAction = settingTurn ? -1 : currentAction + 1
+        setCurrentAction(nextAction);
+        resolve();
+      }, timeout);
+    })
+  }
+
+  async function switchPokemon(ident: PokemonIdent, details: PokemonDetails, hpstatus: PokemonHPStatus) {
+    console.log('Switching', ident, details, hpstatus);
+    
+    return await new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
     });
   }
+  
 
   return (
     <div>
       <div className="flex">
+        
       <BattleCanvas battle={battle} pov={pov}/>
         <div ref={logRef} className="w-1/4 h-[360px] overflow-auto">
           {htmlLog.map((line, index) => (
@@ -300,26 +213,12 @@ export function Game({battleName = 'medalla_doku'}: {battleName?: string}) {
           ))}
         </div>
       </div>
-      <Button onClick={() => playTurn(battle.turn - 1)}>Previous turn</Button>
-      <Button onClick={() => playTurn(battle.turn + 1)}>Next turn</Button>
-      <Button onClick={() => simulateFaint()}>Simulate faint</Button>
-      <Button onClick={() => simulateOtherAttack()}>Simulate attack other side</Button>
+      <Button onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? 'Pause' : 'Play'}</Button>
+      <Input type="number" value={turnInput} onChange={(e) => setTurnInput(parseInt(e.target.value))}/>
+      <Button onClick={() => setCurrentTurn()}>Go to turn</Button>
+      <span className="text-black">{dataLoaded ? 'Yes' : 'Nope'}</span>
     </div>
   );
-
-
-  function playTurn(turn?: number) {
-    if( isPlaying ) return;
-
-    if(turn === undefined){
-      turn = battle.turn;
-    }
-    readTurn(copyBattle(battle), battleLog as string, turn).then(() => {
-    }).catch((error) => {
-      console.error('Error reading log:', error);
-    });
-  }
-
 }
 
 function copyBattle(battle: Battle) {
