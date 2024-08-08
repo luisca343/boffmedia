b2a785de1054133f714cf09d9390448b6c166e30
Luis Carballo <luisca343@gmail.com>
2024-08-08 22:18:06 +0200

    Update battleActions.ts

--- src/app/battlesim/_components/battleActions.ts
+++ src/app/battlesim/_components/battleActions.ts
@@ -4,26 +4,36 @@ import { Scene } from "./Scene";
 
 
 export async function turnAction(currentBattle: Battle, turn: Num) {
-    currentBattle.setTurn(turn);
-    return await new Promise<void>((resolve) => {
-      setTimeout(() => {
+  return await new Promise<void>((resolve) => {
+    setTimeout(() => {
+      
+      resolve();
+    }, 100);
+  });
+}
 
-        resolve();
-      }, 100);
-    });
-  }
+export  async function switchAction(ident: PokemonIdent, details: PokemonDetails, hpstatus: PokemonHPStatus) {
+  return await new Promise<void>((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+}
 
-  export  async function switchAction(ident: PokemonIdent, details: PokemonDetails, hpstatus: PokemonHPStatus) {
-    return await new Promise<void>((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 1000);
-    });
-  }
+export async function moveAction(battle :Battle, scene: Scene | null, attacker: PokemonIdent, moveName: string, defender: PokemonIdent) {
+  const move = battle.get("moves", moveName);
+  if(!scene) return;
+  return await scene.playBattleAnim(move.id, getPokemonIdentCode(attacker) as PokemonIdent, getPokemonIdentCode(defender) as PokemonIdent);
+}
 
-    export async function moveAction(battle :Battle,scene: Scene | null, attacker: PokemonIdent, moveName: string, defender: PokemonIdent) {
-        const move = battle.get("moves", moveName);
-        if(!scene) return;
-        return await scene.playBattleAnim(move.id, attacker.split(':')[0] as PokemonIdent, defender.split(':')[0] as PokemonIdent);
-    }
-  
+export async function damageAction(battle :Battle,scene: Scene | null, ident: PokemonIdent, hpstatus: PokemonHPStatus) {
+  if(!scene) return;
+
+  return scene.showPopup(getPokemonIdentCode(ident) as PokemonIdent, hpstatus.split('/')[0]+'%' as PokemonHPStatus);
+}
+
+
+
+function getPokemonIdentCode(ident: PokemonIdent) {
+  return ident.split(':')[0];
+}
