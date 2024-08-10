6cf3f57918aaa462a51681e3f0b4d395e6ff2b9c
Luis Carballo <luisca343@gmail.com>
2024-08-10 23:24:50 +0200

    :sparkles: Fix Hover Card if pokemon has missing params

--- src/app/battlesim/_components/PokemonDetail.tsx
+++ src/app/battlesim/_components/PokemonDetail.tsx
@@ -2,16 +2,30 @@ import { TypeBadgeSmall } from "@/app/smartrotom/pokedex/entrada/[[...params]]/_
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 import { Pokemon } from "@pkmn/client";
 
-export default function PokemonDetail({pokemon, children, className, key, offset = 20}: 
+export default function PokemonDetail({pokemon, children, className, offset = 20}: 
     {pokemon: Pokemon, children: any, className?: string, key?: string, offset?: number}) {
-    if(!pokemon) return null;
+    let types;
+    try {
+        types = pokemon.types;
+    } catch (error) {
+        console.error('Error accessing pokemon.types:', error);
+        return <HoverCard openDelay={0} closeDelay={0}>
+        <HoverCardTrigger  className={className}>
+            {children}
+        </HoverCardTrigger>
+        <HoverCardContent className="z-[200] bg-slate-800 bg-opacity-90 text-slate-100 w-128" side="top"   style={{zIndex:'999'}}>
+        <div>Failed to load</div>
+        </HoverCardContent>
+    </HoverCard>
+    }
+    if (!pokemon || !types) return <></>;
     return <HoverCard key={pokemon.name} openDelay={0} closeDelay={0}>
-    <HoverCardTrigger  className={className} key={key}>
+    <HoverCardTrigger  className={className}>
         {children}
     </HoverCardTrigger>
-    <HoverCardContent className="z-[200] bg-slate-800 bg-opacity-90 text-slate-100 w-128" side="right" sideOffset={offset}  style={{zIndex:'999'}}>
+    <HoverCardContent className="z-[200] bg-slate-800 bg-opacity-90 text-slate-100 w-128" side="top"   style={{zIndex:'999'}}>
          <span className="font-bold">{pokemon.name}</span> {pokemon.speciesForme} L{pokemon.level}
-         <div className="flex">{pokemon.types.map(type => <TypeBadgeSmall key={type} type={type} />)}</div>
+         <div className="flex">{types.map(type => <TypeBadgeSmall key={type} type={type} />)}</div>
          <br/>
          <div>HP: {pokemon.hp > 0 ? pokemon.hp / pokemon.maxhp * 100 : 0}%</div>
          <span className="flex">Possible abilities: {pokemon.species.abilities[0]} {pokemon.species.abilities[1] && pokemon.species.abilities[1]} {pokemon.species.abilities.H && pokemon.species.abilities.S}</span>
@@ -25,4 +39,4 @@ export default function PokemonDetail({pokemon, children, className, key, offset
 function calculateSpeed(base: number, level: number, natureModifier: number, IV: number, EV: number) {
     const speed = Math.floor((0.01 * (2 * base + IV + Math.floor(0.25 * EV)) * level) + 5) * natureModifier;
     return speed;
-  }
+}
