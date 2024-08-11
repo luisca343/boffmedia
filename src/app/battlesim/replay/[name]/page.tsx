ced1a113d7fc5ae1b07e4da9c61fc910217d1697
Luis Carballo <luisca343@gmail.com>
2024-08-11 19:40:17 +0200

    Por culpa de esto se me ha desequilibrado todo XD

--- src/app/battlesim/replay/[name]/page.tsx
+++ src/app/battlesim/replay/[name]/page.tsx
@@ -10,7 +10,7 @@ const battle = new Battle(new Generations(Dex as any)); // Use const if battle i
 export default function Test({params} : {params: {name: string}}): JSX.Element { // Explicitly type the return value
 
   return (
-    <section className="flex flex-col w-[900px] h-[450px]">
+    <section className="flex flex-col">
       <Game battleName={params.name}/>
     </section>
   );
