e6037ce9dde75448281535d06bb7654492f69dae
Luis Carballo <luisca343@gmail.com>
2024-08-21 17:30:27 +0200

    Update PokemonElement.tsx

--- src/app/battlesim/_components/PokemonElement.tsx
+++ src/app/battlesim/_components/PokemonElement.tsx
@@ -5,6 +5,7 @@ import { Pokemon, Side } from "@pkmn/client";
 import { useSpring, animated, useTransition  } from "@react-spring/web";
 import { getImageSize, getOffset, getScaleMultiplier } from "../_utils/viewUtils";
 import { PokemonImage } from "./PokemonImage";
+import useViewportWidth from "@/services/useViewPortWidth";
 
 type PokemonElementProps = {
     battle: any;
@@ -23,50 +24,14 @@ export type PokemonRefType = {
     export const PokemonElement = forwardRef(({ battle, pokemon, side, position }: PokemonElementProps, ref) => {
         const sideId = side.n % 2 === 0 ? 'p1' : 'p2';
 
-        const initialPosition = {
-            left: getOffset(battle, position, getScaleMultiplier()).left,
-            top: getOffset(battle, position, getScaleMultiplier()).top,
-        };
-        
-        const [props, set] = useSpring(() => ({
-            left: initialPosition.left,
-            top: initialPosition.top,
-            transform: 'translateY(0px)',
-        }));
 
-        
-        const handleBounce = () => {
-            set({ transform: 'translateY(-20px)' });
-            setTimeout(() => set({ transform: 'translateY(0px)' }), 200);
-        };
-
-    
-        useImperativeHandle(ref, () => ({
-            bounce: handleBounce,
-            moveTo: (xTo: number, yTo: number) => {
-                console.log("Moving to", xTo, yTo);
-                const peakX = (initialPosition.left + xTo) / 2;
-                const peakY = initialPosition.top - 100; // Adjust the peak height as needed
-        
-                // Step 1: Jump to the peak of the parabola
-                set({ left: peakX, top: peakY, transform: 'translateY(0px)' });
-                setTimeout(() => {
-                    // Step 2: Fall to the new position
-                    set({ left: xTo, top: yTo, transform: 'translateY(0px)' });
-                    setTimeout(() => {
-                        // Step 3: Return to the initial position
-                        set({ left: initialPosition.left, top: initialPosition.top });
-                    }, 200);
-                }, 200);
-            }
-        }));
-    
         return (
             <animated.div
                 id={position}
                 className="absolute z-50"
                 style={{
-                    ...props,
+                    left:  getOffset(battle, position, getScaleMultiplier()).left,
+                    top: getOffset(battle, position, getScaleMultiplier()).top,
                     width: getImageSize(),
                     height: getImageSize()
                 }}
