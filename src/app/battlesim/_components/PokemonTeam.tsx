166b0883ee5c5f43b7f95880516887c2550d5a8f
Luis Carballo <luisca343@gmail.com>
2024-08-22 11:25:48 +0200

    Update PokemonTeam.tsx

--- src/app/battlesim/_components/PokemonTeam.tsx
+++ src/app/battlesim/_components/PokemonTeam.tsx
@@ -9,7 +9,7 @@ export function PokemonTeam({side}: {side: Side}) {
     return <div className="flex flex-wrap  justify-center items-center text-center w-16 bg-slate-800 bg-opacity-80 r rounded-md z-50">
         {Array.from({length: total}, (_, i) => side.team[i]).map((pokemon, index) => {
             return <PokemonDetail pokemon={pokemon} 
-                    className={`w-6 flex justify-center z-40`} 
+                    className={`flex flex-wrap justify-center z-40`} 
                     key={`pkm-${side.n}-${index}`}>
                 <PokemonSprite pokemon={pokemon} />
             </PokemonDetail>
