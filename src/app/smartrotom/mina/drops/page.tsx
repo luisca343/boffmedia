f839847fbf3a6037330048d1da9715cd0adfec78
Luis Carballo <luisca343@gmail.com>
2024-08-24 20:01:14 +0200

    Update page.tsx

--- src/app/smartrotom/mina/drops/page.tsx
+++ src/app/smartrotom/mina/drops/page.tsx
@@ -1,20 +1,56 @@
 import { rotomGET } from "@/services/boffAPI";
 import MenuWrapper from "../_components/MenuWrapper";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
 
-export default async function Drops(){
-    const drops = await rotomGET('/mine/rewards');
-    return(
+type Drop = {
+    id: number;
+    value: number;
+    name: string;
+    type: string;
+    itemId: string;
+    width: number;
+    height: number;
+}
+
+type DropByType = {
+    [key: string]: {
+        items: Drop[];
+        totalValue: number;
+    }
+}
+
+export default async function Drops() {
+    const {drops, totalValue} = await rotomGET('/mine/rewardsbytype') as {drops: DropByType, totalValue: number};
+    return (
         <MenuWrapper>
             <div>
-                {drops?.map((drop: any, i: number) => {
-                    return (
-                        <div key={i} className="p-4 border rounded shadow  w-auto flex flex-col items-center m-2  bg-main-900 bg-opacity-80">
-                            <p className="text-gray-400">{drop.name}</p>
-                            <p className="text-gray-400">{drop.value}</p>
-                        </div>
-                    )
-                })}
+                {Object.keys(drops).map((type: string) => (
+                    <Collapsible key={type}>
+                        <CollapsibleTrigger className="mx-auto text-2xl p-4 border rounded shadow flex flex-col items-center m-2 bg-slate-900 bg-opacity-95 w-[90%] text-gray-400">
+                            {type} - {getPercentage(drops[type].totalValue, totalValue)}%
+                        </CollapsibleTrigger>
+                        <CollapsibleContent>
+                            <div className="flex flex-wrap justify-center mx-auto w-4/5">
+                                {drops[type].items.map((drop: Drop) => (
+                                    <div key={drop.id} className="text-lg w-1/6 p-4 border rounded shadow flex flex-col items-center m-2 bg-slate-900 bg-opacity-85">
+                                        <img src={`/smartrotom/img/apps/mina/recompensas/${drop.itemId.split(':')[1]}.png`}
+                                            alt={drop.name} 
+                                            className="w-16 h-16 mb-2"
+                                            style={{imageRendering: "pixelated"}}
+                                        />
+                                        <p className="text-gray-400">{drop.name}</p>
+                                        <p className="text-gray-400">{getPercentage(drop.value, totalValue)}%</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </CollapsibleContent>
+                    </Collapsible>
+                ))}
             </div>
         </MenuWrapper>
-    )
+    );
+}
+
+function getPercentage(value: number, total: number) {
+    return (value / total * 100.0).toFixed(3);
 }
