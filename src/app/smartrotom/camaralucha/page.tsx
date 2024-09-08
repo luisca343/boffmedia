bee1e941f12155256b4f92089bf60daa49407b4b
Luis Carballo <luisca343@gmail.com>
2024-09-08 19:55:51 +0200

    Create page.tsx

---
+++ src/app/smartrotom/camaralucha/page.tsx
@@ -0,0 +1,83 @@
+"use client"
+import { Game } from "@/app/battlesim/replay/_components/Game";
+import { getSmartRotomUser } from "@/lib/utils";
+import { rotomGET, rotomPOST } from "@/services/boffAPI"
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react"
+import { PokemonSprite } from "../pokedex/_components/PokemonSprite";
+
+export default function CamaraLucha(){
+    const [combates, setCombates] = useState([])
+    const {data: session} = useSession();
+
+    useEffect(()=>{
+        rotomGET(`/repeticiones/${getSmartRotomUser(session).uuid}`).then((res)=>{
+            setCombates(res)
+        })
+    },[])
+
+    return(
+        <div  className="text-black">
+            <h1>Camara Lucha</h1>
+            <div>
+                {combates.map((combate: any) => (
+                    <div key={combate.id} className="flex">
+                        {combate.side1}
+                        <TeamPreview team={JSON.parse(combate.team1)} />
+                        vs
+                        {combate.team2 &&<TeamPreview team={JSON.parse(combate.team2)} />}
+                        {combate.side2}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+
+function TeamPreview({team}: {team: PokemonData[]}){
+    console.log('team', team)
+    return(
+        <div className="flex flex-row justify-around items-center">
+            {team.map((p, index) => <PokemonSprite 
+                key={index} id={p.dex} form={p.form || 'base'} palette={p.palette || 'none'} 
+                width={50} showStatus={false}/>)}
+        </div>
+    )
+}
+
+
+/*
+
+                {combates.map((combate: any) => (
+                    <Game key={combate.id} />
+                ))}
+
+                {team.map((p, index) => <PokemonSprite key={index} id={p.dex} form={p.form || 'base'} palette={p.palette || 'none'} />)}
+*/
+
+
+interface PokemonData {
+    dex: number;
+    nature: string;
+    species: string;
+    form: string;
+    palette: string;
+    name: string;
+    level: number;
+    item: string;
+    ability: string;
+    moves: string[];
+    ivs: number[];
+    evs: number[];
+    stats: number[];
+}
+
+export interface LogroCombate {
+    npc: string;
+    victoria: boolean;
+    logro: string;
+    team1: PokemonData[];
+    team2: PokemonData[];
+    replay: string;
+  }
