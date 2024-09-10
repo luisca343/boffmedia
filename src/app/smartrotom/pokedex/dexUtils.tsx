3f1790f41c9fb0b485f76663237f111381bb3877
Luis Carballo <luisca343@gmail.com>
2024-09-10 09:25:15 +0200

    Update dexUtils.tsx

--- src/app/smartrotom/pokedex/dexUtils.tsx
+++ src/app/smartrotom/pokedex/dexUtils.tsx
@@ -57,7 +57,7 @@ const typeChart = {
     fighting: { normal: 2, ice: 2, poison: 0.5, flying: 0.5, psychic: 0.5, bug: 0.5, rock: 2, ghost: 0, dark: 2, steel: 2, fairy: 0.5 },
     poison: { grass: 2, poison: 0.5, ground: 0.5, rock: 0.5, ghost: 0.5, steel: 0, fairy: 2 },
     ground: { fire: 2, electric: 2, grass: 0.5, poison: 2, flying: 0, rock: 2, bug: 0.5, steel: 2 },
-    flying: { electric: 0.5, grass: 2, fighting: 2, bug: 0.5, rock: 0.5, steel: 0.5 },
+    flying: { electric: 0.5, grass: 2, fighting: 2, bug: 2, rock: 0.5, steel: 0.5 },
     psychic: { fighting: 2, poison: 2, psychic: 0.5, dark: 0, steel: 0.5 },
     bug: { fire: 0.5, grass: 2, fighting: 0.5, poison: 0.5, flying: 0.5, psychic: 2, ghost: 0.5, dark: 2, steel: 0.5, fairy: 0.5 },
     rock: { fire: 2, ice: 2, fighting: 0.5, ground: 0.5, flying: 2, bug: 2, steel: 0.5 },
@@ -79,6 +79,7 @@ export function getPokemonDefense(type1: string, type2= '') {
     for(let type in typeChart){
         const type1Effectiveness = typeChart[type][type1.toLowerCase()] ?? 1;
         const type2Effectiveness = typeChart[type][type2.toLowerCase()] ?? 1;
+
         result[type] = type1Effectiveness * type2Effectiveness;
     }
     return result
