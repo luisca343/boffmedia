e0cca5cfafc8429f0dfc516db215c9d297f84806
Luis Carballo <luisca343@gmail.com>
2024-09-16 19:00:36 +0200

    Create page.tsx

---
+++ src/app/tools/page.tsx
@@ -0,0 +1,10 @@
+export default function ToolsMain(){
+    return(
+        <div className="flex h-full items-center justify-center bg-main-100">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold text-main-800">Página en construcción</h1>
+          <p className="mt-4 text-lg text-main-600">Esta página está en construcción, por favor regrese más tarde.</p>
+        </div>
+      </div>
+    )
+}
