8e39031bb2bdc37d6eb570ea30e1d9587cf8bbc9
Luis Carballo <luisca343@gmail.com>
2024-09-18 21:15:27 +0200

    Update layout.tsx

--- src/app/smartrotom/starbank/layout.tsx
+++ src/app/smartrotom/starbank/layout.tsx
@@ -1,22 +1,25 @@
-"use client"
+"use client";
 import { SideMenu } from "./_components/SideMenu";
+import TopBar from "./_components/TopBar";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+}
 
-  
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
-    <div className="flex overflow-hidden bg-blue-300 "
-    style={{
-        //background:'url(https://static.vecteezy.com/system/resources/previews/004/695/787/non_2x/banking-service-background-bank-building-icon-made-with-currency-symbols-dollar-euro-yen-and-pound-icons-background-with-currency-signs-bank-financing-money-exchange-illustration-vector.jpg)',
-         backgroundRepeat:'no-repeat', backgroundSize:'cover'}}
-         >
-        <SideMenu />
-      {children}
+    <div
+      className="flex overflow-hidden bg-blue-100 "
+      style={{
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <SideMenu />
+      <div className="h-full w-full overflow-hidden">
+        <TopBar />
+        <div className="h-full w-full">{children}</div>
+      </div>
     </div>
-    
   );
 }
