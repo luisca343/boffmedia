279a7bd0c50a49ce7338edd61de50da656d03689
Luis Carballo <luisca343@gmail.com>
2024-09-21 22:10:50 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,37 +1,41 @@
 export type App = {
-    id: number;
-    name: string;
-    url: string;
-    icon: string;
+  id: number;
+  name: string;
+  url: string;
+  icon: string;
 };
 
-
 export interface SmartRotomUser {
-    username: string;
-    uuid: string;
-    world: null | string; // Assuming world can be either null or a string
-  }
-  
-  export interface User {
-    email: string;
-    username: string;
-    smartRotomUser: SmartRotomUser;
-    iat: number;
-    exp: number;
-    jti: string;
-  }
-  
-  export interface BoffSession {
-    user: User;
-    expires: string; // ISO date string format
-  }
+  username: string;
+  uuid: string;
+  world: null | string; // Assuming world can be either null or a string
+}
 
+export interface User {
+  email: string;
+  username: string;
+  smartRotomUser: SmartRotomUser;
+  iat: number;
+  exp: number;
+  jti: string;
+}
 
-  export interface SmartRotomResponse {
-    status: number;
-    message?: string;
-    error?: string;
-    data?: string;
+export type BoffSession = {
+  user: {
+    username: string | null;
+    email: string | null;
+    smartRotomUser: {
+      username: string;
+      uuid: string;
+    };
+  };
+};
+
+export interface SmartRotomResponse {
+  status: number;
+  message?: string;
+  error?: string;
+  data?: string;
 }
 
 export interface SmartRotomPost {
