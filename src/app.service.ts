3359928b04d71ed6eff20ec589ecb0891d0d7509
Luis Carballo <luisca343@gmail.com>
2024-10-01 22:51:01 +0200

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,8 @@ import { MySQL2Service } from './_utils/MySQL2Service';
 import { PokemonService } from './smartrotom/pokemon/pokemon.service';
 import { LoggingUtil } from './_utils/LoggingUtils';
 
+import { google, sheets_v4 } from 'googleapis';
+
 import fs from 'fs/promises';
 import path from 'path';
 
@@ -47,5 +49,35 @@ export class AppService {
       console.error('Error reading the icons folder:', error);
       return []; // Return an empty array in case of an error
     }
+  }
+
+  async steamKeys() {
+        //AIzaSyCxjks7gBH5U1FtDd_Y1QOvOciSlr1XtQE
+        const auth = new google.auth.GoogleAuth({
+          keyFile: 'boffmedia-a39cdd7a63c7.json',
+          scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+      });
+      
+  
+   
+      const client = await auth.getClient() as any;
+      const sheets = google.sheets({ version: 'v4', auth: client }) as sheets_v4.Sheets;
+   
+         const spreadsheetId = '1mQopLvsmDuz5iHJ9WVN5NLH78UsKrK6E364v3i8a00c';
+          const range = 'A2:D';
+          const response = await sheets.spreadsheets.values.get({
+            spreadsheetId,
+            range,
+          });
+
+          const rows = response.data.values;
+          // We send everything except the "key" column
+          return rows.map((row) => {
+            return {
+              name: row[0],
+              source: row[1],
+              claimed: row[3],
+            };
+          });
   }
 }
