15929cfc4c5c36286cb5c921dc2efbf094c099ec
Luis Carballo <luisca343@gmail.com>
2024-10-03 20:03:47 +0200

    :sparkles: Added toasts to the webpage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {FicusNav} from "@/components/nav/FicusNav";
+import { FicusNav } from "@/components/nav/FicusNav";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const inter = Inter({ subsets: ["latin"] });
-
 
 export const metadata: Metadata = {
   title: "BoffMedia",
@@ -16,14 +17,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col h-screen overflow-hidden`}>
+      <body
+        className={`${inter.className} flex flex-col h-screen overflow-hidden`}
+      >
+        <ToastContainer position="bottom-right" theme="dark" />
         <FicusNav />
-        <section className="overflow-auto border-solid no-scrollbar flex-1">{children}</section>
-        </body>
+        <section className="overflow-auto border-solid no-scrollbar flex-1">
+          {children}
+        </section>
+      </body>
     </html>
   );
 }
-
