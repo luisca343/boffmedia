16b45388c9d8f191d70a8055e8b694c97e9acf90
Luis Carballo <luisca343@gmail.com>
2024-10-12 23:30:36 +0200

    Update create-app.dto.ts

--- src/smartrotom/apps/dto/create-app.dto.ts
+++ src/smartrotom/apps/dto/create-app.dto.ts
@@ -4,12 +4,9 @@ export class CreateAppDto {
   @ApiProperty({ description: 'The name of the app' })
   name: string;
 
-  @ApiProperty({ description: 'The description of the app' })
-  description: string;
-
   @ApiProperty({ description: 'The URL of the app', required: false })
   url?: string;
 
-  @ApiProperty({ description: 'The icon of the app', required: false })
-  icon?: string;
+  @ApiProperty({ description: 'The active status of the app', required: false })
+  active?: number;
 }
