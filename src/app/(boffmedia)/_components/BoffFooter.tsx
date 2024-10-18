33d070b7ea1bf78ff10b59fed823dcf37c0506a5
Luis Carballo <luisca343@gmail.com>
2024-10-18 22:18:17 +0200

    Use new Icons, add new links to Socials

--- src/app/(boffmedia)/_components/BoffFooter.tsx
+++ src/app/(boffmedia)/_components/BoffFooter.tsx
@@ -1,42 +1,74 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Gamepad2, Twitch, Twitter } from "lucide-react";
+import Link from "next/link"
+import { TbBrandBluesky, TbBrandDiscord, TbBrandInstagram, TbBrandTiktok, TbBrandTwitch, TbBrandTwitter, TbBrandYoutube } from "react-icons/tb"
+
+const footerLinks = [
+  { name: "Contacto", link: "/contacto" },
+  {
+    name: "Política de Devoluciones y Cancelaciones",
+    link: "/politicas/devoluciones",
+  },
+  {
+    name: "Política de Reembolsos",
+    link: "/politicas/reembolsos",
+  },
+  {
+    name: "Términos de Servicio",
+    link: "/politicas/terminos",
+  },
+  {
+    name: "Política de Privacidad",
+    link: "/politicas/privacidad",
+  },
+]
+
+const socialLinks = [
+  {
+    name: "Twitter",
+    icon: TbBrandTwitter,
+    href: "https://twitter.com/boffmedia",
+  },
+  {
+    name: "Bluesky",
+    icon: TbBrandBluesky,
+    href: "https://bsky.app/profile/boffmedia.es",
+  },
+  {
+    name: "Instagram",
+    icon: TbBrandInstagram,
+    href: "https://instagram.com/boffmedia",
+  },
+  {
+    name: "YouTube",
+    icon: TbBrandYoutube,
+    href: "https://www.youtube.com/@boffmedia",
+  },
+  {
+    name: "Twitch",
+    icon: TbBrandTwitch,
+    href: "https://twitch.tv/boffmedia",
+  },
+  {
+    name: "TikTok",
+    icon: TbBrandTiktok,
+    href: "https://tiktok.com/@boffmedia",
+  },
+  {
+    name: "Discord",
+    icon: TbBrandDiscord,
+    href: "https://discord.gg/RjhVY2eK9m",
+  },
+]
 
 export function BoffFooter() {
   return (
     <footer className="bg-gray-800 text-orange-100 py-8">
       <div className="container mx-auto px-4 text-center">
-        <Image
-          src="/img/boff.png"
-          alt="Logo de BoffMedia"
-          width={100}
-          height={100}
-          className="mx-auto mb-6 drop-shadow-glow"
-        />
         <p className="text-xl mb-4">
           &copy; 2024 BoffMedia. Todos los derechos reservados.
         </p>
         <nav>
-          <ul className="flex justify-center space-x-6">
-            {[
-              { name: "Contacto", link: "/contacto" },
-              {
-                name: "Política de Devoluciones y Cancelaciones",
-                link: "/politicas/devoluciones",
-              },
-              {
-                name: "Política de Reembolsos",
-                link: "/politicas/reembolsos",
-              },
-              {
-                name: "Términos de Servicio",
-                link: "/politicas/terminos",
-              },
-              {
-                name: "Política de Privacidad",
-                link: "/politicas/privacidad",
-              },
-            ].map((item) => (
+          <ul className="flex flex-wrap justify-center gap-4">
+            {footerLinks.map((item) => (
               <li key={item.name}>
                 <Link
                   href={item.link}
@@ -48,30 +80,19 @@ export function BoffFooter() {
             ))}
           </ul>
         </nav>
-        <div className="flex justify-center space-x-6 mt-6">
-          <Link
-            href="https://twitter.com/boffmedia"
-            className="text-orange-300 hover:text-orange-400 transition duration-300"
-          >
-            <Twitter className="w-8 h-8" />
-            <span className="sr-only">Síguenos en Twitter</span>
-          </Link>
-          <Link
-            href="https://twitch.tv/boffmedia"
-            className="text-orange-300 hover:text-orange-400 transition duration-300"
-          >
-            <Twitch className="w-8 h-8" />
-            <span className="sr-only">Síguenos en Twitch</span>
-          </Link>
-          <Link
-            href="https://discord.gg/boffmedia"
-            className="text-orange-300 hover:text-orange-400 transition duration-300"
-          >
-            <Gamepad2 className="w-8 h-8" />
-            <span className="sr-only">Únete a nuestro Discord</span>
-          </Link>
+        <div className="flex justify-center item space-x-4 mt-4">
+          {socialLinks.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className="text-orange-300 hover:text-orange-400 transition duration-300"
+            >
+              <item.icon className="w-8 h-8" />
+              <span className="sr-only">Síguenos en {item.name}</span>
+            </Link>
+          ))}
         </div>
       </div>
     </footer>
-  );
+  )
 }
