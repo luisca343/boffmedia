72a145bc5080de672b65afb8b8e5469dd8abb4b3
Luis Carballo <luisca343@gmail.com>
2024-11-06 23:53:11 +0100

    Added config and data access

--- src/boffmedia/herramientas/ptcgp/ptcgp.module.ts
+++ src/boffmedia/herramientas/ptcgp/ptcgp.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PtcgpService } from './ptcgp.service';
 import { PtcgpController } from './ptcgp.controller';
+import { ConfigService } from '../../../config.service';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [PtcgpController],
-  providers: [PtcgpService],
+  providers: [PtcgpService, ConfigService],
 })
 export class PtcgpModule {}
