ae3173090e1b04d6e031e40c8703c1bbdab2ea83
Luis Carballo <luisca343@gmail.com>
2024-11-16 17:50:20 +0100

    Update ProbabilityTable.tsx

--- src/app/(boffmedia)/(herramientas)/pokemon/tcgpocket/_components/ProbabilityTable.tsx
+++ src/app/(boffmedia)/(herramientas)/pokemon/tcgpocket/_components/ProbabilityTable.tsx
@@ -1,28 +1,31 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { AllPackProbabilities } from '../types'
 import PercentageToDecimal from "./PercentageToDecimal"
+import { useTranslations } from "next-intl"
 
 interface ProbabilityTableProps {
   probabilities: AllPackProbabilities
 }
 
 export function ProbabilityTable({ probabilities }: ProbabilityTableProps) {
-    console.log(probabilities)
+    const trans = useTranslations('tcgpocket')
   return (
     <Table>
       <TableHeader>
         <TableRow>
           <TableHead>Pack Name</TableHead>
-          <TableHead>1 Card</TableHead>
-          <TableHead>2 Cards</TableHead>
-          <TableHead>3 Cards</TableHead>
-          <TableHead>4+ Cards</TableHead>
-          <TableHead>Aggregate</TableHead>
+          <TableHead>Carta 1</TableHead>
+          <TableHead>Carta 2</TableHead>
+          <TableHead>Carta 3</TableHead>
+          <TableHead>Carta 4</TableHead>
+          <TableHead>Carta 5</TableHead>
+          <TableHead>Total</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {Object.entries(probabilities).map(([packName, packProbabilities]) => (
           <TableRow key={packName}>
+            <TableCell>{trans(`packs.${packName}`)}</TableCell>
             {packProbabilities.newCardProbabilities.map((prob: number, index: number) => (
               <TableCell key={index}>
                 <PercentageToDecimal value={prob * 100} />
