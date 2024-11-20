59b2665d7e536189877e80f44f66bf9f75d29204
Luis Carballo <luisca343@gmail.com>
2024-11-20 23:18:31 +0100

    Update BoffFooter.tsx

--- src/app/(boffmedia)/_components/BoffFooter.tsx
+++ src/app/(boffmedia)/_components/BoffFooter.tsx
@@ -3,6 +3,7 @@ import { TbBrandBluesky, TbBrandDiscord, TbBrandInstagram, TbBrandTiktok, TbBran
 
 const footerLinks = [
   { name: "Contacto", link: "/contacto" },
+  /*
   {
     name: "Política de Devoluciones y Cancelaciones",
     link: "/politicas/devoluciones",
@@ -10,7 +11,7 @@ const footerLinks = [
   {
     name: "Política de Reembolsos",
     link: "/politicas/reembolsos",
-  },
+  },*/
   {
     name: "Términos de Servicio",
     link: "/politicas/terminos",
@@ -61,36 +62,38 @@ const socialLinks = [
 
 export function BoffFooter() {
   return (
-    <footer className="bg-surface-800 text-primary-100 py-8">
-      <div className="container mx-auto px-4 text-center">
-        <p className="text-xl mb-4">
-          &copy; 2024 BoffMedia. Todos los derechos reservados.
-        </p>
-        <nav>
-          <ul className="flex flex-wrap justify-center gap-4">
-            {footerLinks.map((item) => (
-              <li key={item.name}>
-                <Link
-                  href={item.link}
-                  className="text-primary-300 hover:text-primary-400 transition duration-300"
-                >
-                  {item.name}
-                </Link>
-              </li>
+    <footer className="bg-surface-800 text-primary-100 py-6">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
+          <div className="text-center md:text-left">
+            <p className="text-sm">&copy; 2024 BoffMedia. Todos los derechos reservados.</p>
+          </div>
+          <nav className="order-last md:order-none">
+            <ul className="flex flex-wrap justify-center gap-2 text-xs">
+              {footerLinks.map((item) => (
+                <li key={item.name}>
+                  <Link
+                    href={item.link}
+                    className="text-primary-300 hover:text-primary-400 transition duration-300"
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="flex justify-center md:justify-end space-x-3">
+            {socialLinks.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-primary-300 hover:text-primary-400 transition duration-300"
+              >
+                <item.icon className="w-6 h-6" />
+                <span className="sr-only">Síguenos en {item.name}</span>
+              </Link>
             ))}
-          </ul>
-        </nav>
-        <div className="flex justify-center item space-x-4 mt-4">
-          {socialLinks.map((item) => (
-            <Link
-              key={item.name}
-              href={item.href}
-              className="text-primary-300 hover:text-primary-400 transition duration-300"
-            >
-              <item.icon className="w-8 h-8" />
-              <span className="sr-only">Síguenos en {item.name}</span>
-            </Link>
-          ))}
+          </div>
         </div>
       </div>
     </footer>
