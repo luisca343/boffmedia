83e11f66b2a8edf2fab9833eeeeae05cda6aa5f4
Luis Carballo <luisca343@gmail.com>
2024-12-17 18:50:27 +0100

    [Testing] Events

---
+++ src/boffmedia/events/events.module.ts
@@ -0,0 +1,12 @@
+/*
+import { Module } from '@nestjs/common';
+import { EventsController } from './events.controller';
+import { EventsService } from './events.service';
+import { MySQL2Service } from '@/_utils/MySQL2Service';
+
+@Module({
+  controllers: [EventsController],
+  providers: [EventsService, MySQL2Service]
+})
+export class EventsModule {}
+*/
