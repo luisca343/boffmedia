89ee168c138240f79822968c114e9c9f912d4d57
Luis Carballo <luisca343@gmail.com>
2025-01-10 18:41:57 +0100

    Update smartrotom.controller.ts

--- src/smartrotom/_main/smartrotom.controller.ts
+++ src/smartrotom/_main/smartrotom.controller.ts
@@ -27,25 +27,25 @@ export class SmartrotomController {
     private readonly responseService: ResponseService,
   ) {}
 
-  @Post('achievements')
-  @ApiOperation({ summary: 'Get achievements for a player' })
+  @Post('battle-achievements')
+  @ApiOperation({ summary: 'Get battle achievements for a player' })
   @ApiResponse({
     status: HttpStatus.OK,
-    description: 'Achievements retrieved successfully.',
+    description: 'Battle achievements retrieved successfully.',
   })
   @ApiResponse({
     status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Failed to retrieve achievements.',
+    description: 'Failed to retrieve battle achievements.',
   })
   @ApiBody({ type: UuidDto })
   async getAchievements(@Body() { uuid }: { uuid: string }) {
-    const action = 'get achievements';
+    const action = 'get battle achievements';
     try {
       this.responseService.logRequest(action, { uuid });
       const achievements = await this.smartrotomService.getAchievements(uuid);
       this.responseService.logSuccess(action, achievements);
       return this.responseService.createSuccessResponse(
-        'Achievements retrieved successfully',
+        'Battle achievements retrieved successfully',
         achievements,
       );
     } catch (error) {
@@ -53,15 +53,15 @@ export class SmartrotomController {
     }
   }
 
-  @Get('achievement/:uuid/:achievementId')
+  @Get('battle-achievement/:uuid/:achievementId')
   @ApiOperation({ summary: 'Get a specific achievement for a player' })
   @ApiResponse({
     status: HttpStatus.OK,
-    description: 'Achievement retrieved successfully.',
+    description: 'Battle chievement retrieved successfully.',
   })
   @ApiResponse({
     status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Failed to retrieve achievement.',
+    description: 'Failed to retrieve battle achievement.',
   })
   async getAchievementForPlayer(
     @Param('uuid') uuid: string,
@@ -231,6 +231,57 @@ export class SmartrotomController {
     }
   }
 
+  @Get('regions')
+  @ApiOperation({ summary: 'Get regions' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Regions retrieved successfully.',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Failed to retrieve regions.',
+  })
+  async getRegions() {
+    const regions = await axios.get(`${process.env.WINGULL_API}/regions`);
+
+    const townColors = {
+      ARRECIFE_WINGULL: { fill: 0x5500bfff, border: 0x00bfff },
+      PUERTO_WINGULL: { fill: 0x550077be, border: 0x0077be },
+      PUEBLO_TULIPAN: { fill: 0x5532cd32, border: 0x32cd32 },
+      PUEBLO_SHIROI: { fill: 0x55ffffff, border: 0xffffff },
+      PUEBLO_TAKAI: { fill: 0x55e0ffff, border: 0xe0ffff },
+      PUEBLO_HAGANE: { fill: 0x55808080, border: 0x808080 },
+      PUEBLO_DENTO: { fill: 0x556a5acd, border: 0x6a5acd },
+      PUEBLO_IWA: { fill: 0x55a9a9a9, border: 0xa9a9a9 },
+      PUEBLO_TSUCHI: { fill: 0x55d2691e, border: 0xd2691e },
+      PUEBLO_OASIS: { fill: 0x55f4a460, border: 0xf4a460 },
+      PUEBLO_SENSHI: { fill: 0x55b22222, border: 0xb22222 },
+      PUEBLO_KINOKO: { fill: 0x55ff69b4, border: 0xff69b4 },
+      PUEBLO_SAKURA: { fill: 0xffffb7c5, border: 0xffb7c5 },
+      PUEBLO_DOKU: { fill: 0x55800080, border: 0x800080 },
+      PUEBLO_GAKU: { fill: 0x55f4a460, border: 0xf4a460 },
+      PUEBLO_LAVANDA: { fill: 0x55483d8b, border: 0x483d8b },
+      PUEBLO_DENKI: { fill: 0x55ffff00, border: 0xffff00 },
+      PUEBLO_MIZU: { fill: 0x554169e1, border: 0x4169e1 },
+      PUEBLO_OLIVO: { fill: 0x55556b2f, border: 0x556b2f },
+      NARUKAMI: { fill: 0x55ffd700, border: 0xffd700 },
+      AKINA: { fill: 0x55ff4500, border: 0xff4500 },
+      FUKITSU: { fill: 0x55000000, border: 0x000000 },
+      GANSOLIA: { fill: 0x55deb887, border: 0xdeb887 },
+    };
+
+    const regionsWithColors = regions.data.map((region: any) => {
+      const color = townColors[region.name.toUpperCase()];
+      if (color) {
+        region.fillColor = color.fill & 0xffffffff;
+        region.strokeColor = color.border & 0xffffffff;
+      }
+      return region;
+    });
+
+    return regionsWithColors;
+  }
+
   /**
    * Battle Section
    *
@@ -315,57 +366,5 @@ export class SmartrotomController {
     } catch (error) {
       this.responseService.handleError(action, error, { npcConfigName });
     }
-  }
-
-
-
-  @Get('regions')
-  @ApiOperation({ summary: 'Get regions' })
-  @ApiResponse({status: HttpStatus.OK, description: 'Regions retrieved successfully.', })
-  @ApiResponse({
-    status: HttpStatus.INTERNAL_SERVER_ERROR,
-    description: 'Failed to retrieve regions.',
-  })
-  async getRegions() {
-    const regions = await axios.get(`${process.env.WINGULL_API}/regions`);
-
-    const townColors = {
-      'ARRECIFE_WINGULL': { fill: 0x5500BFFF, border: 0x00BFFF },
-      'PUERTO_WINGULL': { fill: 0x550077BE, border: 0x0077BE },
-      'PUEBLO_TULIPAN': { fill: 0x5532CD32, border: 0x32CD32 },
-      'PUEBLO_SHIROI': { fill: 0x55FFFFFF, border: 0xFFFFFF },
-      'PUEBLO_TAKAI': { fill: 0x55E0FFFF, border: 0xE0FFFF },
-      'PUEBLO_HAGANE': { fill: 0x55808080, border: 0x808080 },
-      'PUEBLO_DENTO': { fill: 0x556A5ACD, border: 0x6A5ACD },
-      'PUEBLO_IWA': { fill: 0x55A9A9A9, border: 0xA9A9A9 },
-      'PUEBLO_TSUCHI': { fill: 0x55D2691E, border: 0xD2691E },
-      'PUEBLO_OASIS': { fill: 0x55F4A460, border: 0xF4A460 },
-      'PUEBLO_SENSHI': { fill: 0x55B22222, border: 0xB22222 },
-      'PUEBLO_KINOKO': { fill: 0x55FF69B4, border: 0xFF69B4 },
-      'PUEBLO_SAKURA': { fill: 0xFFFFB7C5, border: 0xFFB7C5 },
-      'PUEBLO_DOKU': { fill: 0x55800080, border: 0x800080 },
-      'PUEBLO_GAKU': { fill: 0x55F4A460, border: 0xF4A460 },
-      'PUEBLO_LAVANDA': { fill: 0x55483D8B, border: 0x483D8B },
-      'PUEBLO_DENKI': { fill: 0x55FFFF00, border: 0xFFFF00 },
-      'PUEBLO_MIZU': { fill: 0x554169E1, border: 0x4169E1 },
-      'PUEBLO_OLIVO': { fill: 0x55556B2F, border: 0x556B2F },
-      'NARUKAMI': { fill: 0x55FFD700, border: 0xFFD700 },
-      'AKINA': { fill: 0x55FF4500, border: 0xFF4500 },
-      'FUKITSU': { fill: 0x55000000, border: 0x000000 },
-      'GANSOLIA': { fill: 0x55DEB887, border: 0xDEB887 },
-    };
-    
-    const regionsWithColors = regions.data.map((region: any) => {
-      const color = townColors[region.name.toUpperCase()];
-      if (color) {
-        region.fillColor = color.fill & 0xFFFFFFFF;
-        region.strokeColor = color.border & 0xFFFFFFFF;
-      }
-      return region;
-    });
-
-  
-    
-    return regionsWithColors;
   }
 }
