de8d1430c784e1165f25c3c162adf115c0414bac
Luis Carballo <luisca343@gmail.com>
2025-03-29 00:21:09 +0100

    Update PlayerGalleryHeader.tsx

--- src/app/(boffmedia)/(herramientas)/pokemon/tcgpocket/_components/PlayerGalleryHeader.tsx
+++ src/app/(boffmedia)/(herramientas)/pokemon/tcgpocket/_components/PlayerGalleryHeader.tsx
@@ -40,7 +40,7 @@ export function PlayerGalleryHeader({
   setShowAmounts,
   onRecentUpdatesClick
 }: PlayerGalleryHeaderProps) {
-  const t = useTranslations('tools.tcgpocket')
+  const t = useTranslations('tcgpocket')
   return (
     <div className="space-y-4 sm:space-y-6">
       <div className="text-center">
