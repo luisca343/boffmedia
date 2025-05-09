87f7e0efba3b1edcea2ee3c53d6a1d8309bb2150
Luis Carballo <luisca343@gmail.com>
2025-05-09 13:50:13 +0200

    z index fixes

--- src/app/battlesim/calc/_components/DamageResults.tsx
+++ src/app/battlesim/calc/_components/DamageResults.tsx
@@ -8,27 +8,19 @@ interface DamageResultsProps {
 
 export default function DamageResults({ result }: DamageResultsProps) {
   if (!result) return null;
-  
   const { 
     title,
-    damageAmounts,
-    minPercent,
-    maxPercent
+    damageAmounts
   } = getDamageText(result);
   
   
   return (
     <div className="border border-surface-700 rounded px-2 py-1.5 bg-surface-700 text-xs">
-      <h3 className="font-medium text-primary-300 mb-0.5">{title}</h3>
-      
-      <div className="mb-0.5 text-surface-400">
-        <span className="mr-1">Damage:</span>
-        <span className="font-medium text-surface-200">{minPercent}% - {maxPercent}%</span>
-      </div>
+      <span className="font-medium text-primary-300 mb-0.5 text-base">{title}</span>
       
       <div className="text-surface-400 text-xs">
         <span className="mr-1">Possible damage:</span>
-        <span className="font-medium text-surface-200">{damageAmounts}</span>
+        <span className="font-medium text-surface-200">({damageAmounts})</span>
       </div>
     </div>
   );
