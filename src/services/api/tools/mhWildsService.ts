0243894f9f4ffe4c99386b5bed19d79436e97847
Luis Carballo <luisca343@gmail.com>
2025-06-09 23:27:14 +0200

    Update mhWildsService.ts

--- src/services/api/tools/mhWildsService.ts
+++ src/services/api/tools/mhWildsService.ts
@@ -2,10 +2,10 @@ import { ArmorPiece, Charm, Decoration, Weapon } from "@/types/tools/mhwilds";
 import { apiGET } from "@/services/boffAPI";
 
 export const mhWildsService = {
-    getWeapons: (locale: string) => apiGET<Weapon[]>(`/tools/mhwilds/weapons/${locale}`),
-    getArmor: (locale: string) => apiGET<ArmorPiece[]>(`/tools/mhwilds/armor/${locale}`),
-    getCharms: (locale: string) => apiGET<Charm[]>(`/tools/mhwilds/ranks/${locale}`),
-    getDecorations: (locale: string) => apiGET<Decoration[]>(`/tools/mhwilds/decorations/${locale}`),
-    getSkills: (locale: string) => apiGET<any[]>(`/tools/mhwilds/skills/${locale}`),
-    getWeaponTree: (locale: string) => apiGET<any>(`/tools/mhwilds/weapon-tree/${locale}`),
+    getWeapons: (locale: string) => apiGET<Weapon[]>(`/herramientas/mhwilds/weapons/${locale}`),
+    getArmor: (locale: string) => apiGET<ArmorPiece[]>(`/herramientas/mhwilds/armor/${locale}`),
+    getCharms: (locale: string) => apiGET<Charm[]>(`/herramientas/mhwilds/ranks/${locale}`),
+    getDecorations: (locale: string) => apiGET<Decoration[]>(`/herramientas/mhwilds/decorations/${locale}`),
+    getSkills: (locale: string) => apiGET<any[]>(`/herramientas/mhwilds/skills/${locale}`),
+    getWeaponTree: (locale: string) => apiGET<any>(`/herramientas/mhwilds/weapon-tree/${locale}`),
 };
