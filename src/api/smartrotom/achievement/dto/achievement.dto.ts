056354d729fb86b21780640083fd51d141dbb9f9
Luis Carballo <luisca343@gmail.com>
2025-08-18 14:54:40 +0200

    Daily rewards extracted to external file

--- src/api/smartrotom/achievement/dto/achievement.dto.ts
+++ src/api/smartrotom/achievement/dto/achievement.dto.ts
@@ -1,7 +1,8 @@
+import { BaseDto } from '@api/_utils/dto/base.dto';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
-export class GetAchievementsDto {
+export class GetAchievementsDto extends BaseDto {
   @ApiProperty({ 
     description: 'Player UUID',
     example: '67d9b543-5ac9-41e1-a8a5-20d7689e24a4'
