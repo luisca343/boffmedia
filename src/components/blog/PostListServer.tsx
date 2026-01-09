c9cec0720c8ebb9faf073caa83eef0081e2b65d5
Luis Carballo <luisca343@gmail.com>
2026-01-09 22:35:34 +0100

    Remove PostListClient and PostListServer components
    Deleted PostListClient.tsx and PostListServer.tsx from the blog components directory. These components are no longer needed, possibly due to refactoring or consolidation of post listing logic.

--- src/components/blog/PostListServer.tsx
+++
@@ -1,9 +0,0 @@
-import PostList from './PostList'
-import { getLocale } from 'next-intl/server'
-import { getPosts } from '@/app/(blog)/blog/get-posts'
-
-export default async function PostListServer({ localeProp }: { localeProp?: string }) {
-  const locale = localeProp || await getLocale()
-  const posts = await getPosts(locale)
-  return <PostList posts={posts} locale={locale} />
-}
