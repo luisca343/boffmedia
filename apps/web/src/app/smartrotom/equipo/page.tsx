1a32d90c64611631613a480b0dc80fb9ee7d70d5
Luisca <luisca343@gmail.com>
2026-05-31 10:26:07 +0200

    feat(smartrotom): add new pages for Album, Equipo, Eventos, Gimnasios, and Torneos

---
+++ apps/web/src/app/smartrotom/equipo/page.tsx
@@ -0,0 +1,7 @@
+export default function Equipo() {
+  return (
+    <div className="bg-surface-800 min-h-full overflow-auto p-4">
+      <h1 className="text-white text-2xl font-bold">Equipo</h1>
+    </div>
+  );
+}
